adde934cbefbff1a1ae4c5232778366f41b0c992
pontikos <npontikos@gmail.com>
2018-12-31 08:43:42 +0000

    rm home.py

--- views/__init__.py
+++ views/__init__.py
@@ -92,6 +92,7 @@ app.config.from_object(__name__)
 sess=Session()
 sess.init_app(app)
 
+
 def sqlite3_ro_cursor(dbname):
    fd = os.open(dbname, os.O_RDONLY)
    conn = sqlite3.connect('/dev/fd/%d' % fd)
@@ -1188,13 +1189,56 @@ def peek(iterable):
         return None
     return first, itertools.chain([first], iterable)
 
+@app.route('/register',methods=['POST'])
+def register():
+    name=request.form.get('name').replace(' ','')
+    affiliation=request.form.get('affiliation')
+    email=request.form.get('email')
+    groups=request.form.getlist('group[]')
+    user=orm.User(user_db=get_db(app.config['DB_NAME_USERS']),user=name,groups=groups,email=email,affiliation=affiliation)
+    print(user.json())
+    print(user.status)
+    return jsonify(message=user.status['message']), user.status['http_code']
+
+
+@app.route('/', methods=['GET'])
+def homepage():
+    db=get_db()
+    patients_db=get_db(app.config['DB_NAME_PATIENTS']) 
+    total_variants=db.variants.count()
+    total_variants=db.variants.count()
+    print('total_variants',total_variants,)
+    total_patients=patients_db.patients.count()
+    print('total_patients',total_patients,)
+    male_patients=patients_db.patients.find( {'sex':'M'}).count()
+    print('male_patients',male_patients,)
+    female_patients=patients_db.patients.find( {'sex':'F'}).count()
+    print('female_patients',female_patients,)
+    unknown_patients=patients_db.patients.find( {'sex':'U'}).count()
+    hpo_json={}
+    exac_variants=0
+    print('exac_variants',exac_variants,)
+    pass_variants=db.variants.find({'FILTER':'PASS'}).count()
+    print('pass_variants',pass_variants,)
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    pass_exac_variants=0
+    print('pass_exac_variants',pass_exac_variants,)
+    #pass_exac_variants=db.variants.find({'in_exac':True,'filter':'PASS'}).count()
+    pass_exac_variants=0
+    #nonexac_variants=db.variants.find({'in_exac':False}).count()
+    nonexac_variants=0
+    #pass_nonexac_variants=db.variants.find({'in_exac':False,'filter':'PASS'}).count()
+    pass_nonexac_variants=0
+    nonpass_variants=(total_variants-pass_variants)
+    nonpass_nonexac_variants=nonexac_variants-pass_nonexac_variants
+
 import views.my_patients
 import views.gene
 import views.variant
 import views.individual
 import views.hpo
 import views.search
-import views.home
 
 
 
