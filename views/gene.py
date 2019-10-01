b3577fd6f7d34b7058af6d576b3c401405be6b4b
pontikos <npontikos@gmail.com>
2019-10-01 16:14:23 +0100

    move variant and gene endpoints to bigquery

--- views/gene.py
+++ views/gene.py
@@ -7,23 +7,22 @@ from views import *
 @app.route('/gene/<gene_id>/<subset>')
 @requires_auth
 def gene(gene_id, subset='all', language='en'):
-   x=json.loads(file(app.config['USER_CONFIGURATION'].format(session['user'],language,'gene') ,'r').read())
+   x=json.loads(open(app.config['USER_CONFIGURATION'].format(session['user'],language,'gene') ,'r').read())
    #c,fd,=sqlite3_ro_cursor(app.config['PHENOPOLIS_DB'])
    gene_id=gene_id.upper()
    if gene_id.startswith('ENSG'):
-      q="SELECT * FROM `poised-breaker-236510.phenopolis_August2019.genes` where gene_symbol='%s'" % gene_id
+      q="SELECT * FROM `poised-breaker-236510.phenopolis_August2019.genes` where gene_id='%s'" % gene_id
       query_job=bigquery_client.query(q, location="EU",) 
-      data=[x for x in query_job]
+      data=[dict(x) for x in query_job]
    else:
       q="SELECT * FROM `poised-breaker-236510.phenopolis_August2019.genes` where gene_name='%s'" % gene_id
       query_job=bigquery_client.query(q, location="EU",) 
-      data=[x for x in query_job]
+      data=[dict(x) for x in query_job]
    if not data:
       q="SELECT * FROM `poised-breaker-236510.phenopolis_August2019.genes` where other_names like '%s'" % ('%'+gene_id+'%')
       query_job=bigquery_client.query(q, location="EU",) 
-      data=[x for x in query_job]
-   x[0]['metadata']['data']=[dict(zip([h[0] for h in c.description],r)) for r in data]
-   print x[0]['metadata']['data']
+      data=[dict(x) for x in query_job]
+   x[0]['metadata']['data']=data
    chrom=x[0]['metadata']['data'][0]['chrom']
    start=x[0]['metadata']['data'][0]['start']
    stop=x[0]['metadata']['data'][0]['stop']
@@ -45,23 +44,30 @@ def gene(gene_id, subset='all', language='en'):
                 {"display": "Wikigenes","href": "http://www.wikigenes.org/?search="+gene_name},
                 {"display": "GTEx (expression)","href": "http://www.gtexportal.org/home/gene/"+gene_name}
                ]
-       d["related_hpo"]=[ {'display':c.execute("select hpo_name from hpo where hpo_id='%s' limit 1"%hpo_id).fetchone()[0], 'end_href':hpo_id} for hpo_id, in c.execute("select hpo_id from gene_hpo where gene_symbol='%s'"%gene_name).fetchall() ]
-   #c.execute("select * from variants where gene_symbol=?",(x[0]['metadata']['data'][0]['gene_name'],))
-   q="select * from variants where gene_symbol='%s'" % (x[0]['metadata']['data'][0]['gene_name'],)
+       q="select hpo_id from `poised-breaker-236510.phenopolis_August2019.gene_hpo` where gene_symbol='%s'"%gene_name
+       query_job=bigquery_client.query(q, location="EU",) 
+       data=[dict(x) for x in query_job]
+       d["related_hpo"]=[]
+       for x in []:
+           hpo_id=x['hpo_id']
+           print(hpo_id)
+           q="select hpo_name from `poised-breaker-236510.phenopolis_August2019.hpo` where hpo_id='%s' limit 1"%hpo_id
+           query_job=bigquery_client.query(q, location="EU",) 
+           data=[dict(x) for x in query_job][0]
+           d['related_hpo']+=[{'display':data['hpo_name'], 'end_href':hpo_id}]
+   q="select * from `poised-breaker-236510.phenopolis_August2019.variants` where gene_symbol='%s'" % (x[0]['metadata']['data'][0]['gene_name'],)
    query_job=bigquery_client.query(q, location="EU",) 
-   data=[x for x in query_job]
+   data=[dict(x) for x in query_job]
    x[0]['variants']['data']=data
    cadd_gt_20=0
    for v in x[0]['variants']['data']:
        v['variant_id']=[{'display':'%s-%s-%s-%s' % (v['CHROM'], v['POS'], v['REF'], v['ALT'],)}]
-       if v['cadd_phred'] and float(v['cadd_phred'])>=20: cadd_gt_20+=1
+       if v['cadd_phred'] and v['cadd_phred']!='NA' and float(v['cadd_phred'])>=20: cadd_gt_20+=1
    x[0]['preview']=[['pLI', x[0]['metadata']['data'][0].get('pLI','')],['Number of variants',len(x[0]['variants']['data'])],['CADD > 20',cadd_gt_20]]
-   sqlite3_ro_close(c,fd)
    for d in x[0]['metadata']['data']: d['number_of_variants']=len(x[0]['variants']['data'])
    process_for_display(x[0]['variants']['data'])
-   print x[0]['preview']
+   print(x[0]['preview'])
    #print x[0]['variants']['data'][0]
-   if session['user']=='demo': x[0]['variants']['data']=[]
    if subset=='all': return json.dumps(x)
    else: return json.dumps([{subset:y[subset]} for y in x])
     
