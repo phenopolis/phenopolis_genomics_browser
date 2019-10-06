9e39e4f809069d6a127934626c0fb32750780e75
pontikos <npontikos@gmail.com>
2019-10-06 19:16:11 +0100

    save user config to db

--- views/save_configuration.py
+++ views/save_configuration.py
@@ -9,7 +9,8 @@ def save_configuration(pageType,pagePart,language='en'):
     print(pageType)
     print(pagePart)
     if pageType=='my_patients': pageType='hpo'
-    x=c.fetchone()
+    x=c.fetchone()[0]
+    print(x)
     for col in x[0][pagePart]['colNames']:
         if col['key'] in request.form.getlist('colNames[]'):
             print(col['key'], True)
@@ -17,7 +18,7 @@ def save_configuration(pageType,pagePart,language='en'):
         else:
             print(col['key'], False)
             col['default']=False
-    c.execute("UPDATE user_config SET config=%s WHERE user_name=%s AND language=%s AND page=%s",(json.dumps(x),u,lang,page))
+    c.execute("UPDATE user_config SET config=%s WHERE user_name=%s AND language=%s AND page=%s",(json.dumps(x),session['user'],language,pageType))
     conn.commit()
     return jsonify(success=''), 200
 
