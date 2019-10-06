66449b14d535f081b42cf14e334c4e5ca0bcf3fc
IsmailM <ismail.moghul@gmail.com>
2019-10-06 16:43:42 +0100

    use defaults to help beanstalk

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
 from views import *
 
 # Load default config and override config from an environment variable
-application.config.from_pyfile('../dev.cfg')
+# application.config.from_pyfile('../dev.cfg')
 
 if __name__ == "__main__":
-    application.run(host='0.0.0.0',port=7888,threaded=True)
+    application.run()
 
 
 
