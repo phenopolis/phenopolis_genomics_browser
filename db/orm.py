5ee5acdacc4e7eab1e86fd1ebf8ed7aaf36b58dd
catherineaok <cokuboyejo@gmail.com>
2019-10-27 16:34:43 +0000

    begin creating tables

--- db/orm.py
+++ db/orm.py
@@ -37,9 +37,69 @@ CREATE TABLE public.genes (
     gene_name text
 );
 """
+
+## ??how to store strings as char vs varchar (varchar by default)
+
 class Gene(Base):
     __tablename__ = 'genes'
     id   = Column(Integer, primary_key=True)
+    stop = Column('stop', String(255)) 
+    gene_id = Column('gene_id', String(255)) 
+    chrom =  Column('stop', String(2))
+    strand = Column('strand', String(1)) # forward or reverse strand
+    full_gene_name = Column('full_gene_name', String(255)) 
+    gene_name_upper = Column('gene_name_upper', String(255))
+    other_names = Column('other_names', String(255))
+    canonical_transcript = Column('canonical_transcript', String(255))
+    start = Column('start', String(255))
+    xstop = Column('xstop', String(255))
+    xstart = Column('xstart', String(255))
+    gene_name = Column('gene_name', String(255))
+
+"""
+CREATE TABLE variants(
+  `#CHROM` TEXT,
+  `POS` INTEGER,
+  `ID` TEXT,
+  `REF` TEXT,
+  `ALT` TEXT,
+  `AF` REAL,
+  `AC` INTEGER,
+  `AN` INTEGER,
+  `HET_COUNT` INTEGER,
+  `HOM_COUNT` INTEGER,
+  `DP` INTEGER,
+  `FS` INTEGER,
+  `MLEAC` INTEGER,
+  `MLEAF` INTEGER,
+  `MQ` INTEGER,
+  `FILTER` TEXT,
+  `HET` TEXT,
+  `HOM` TEXT,
+  `most_severe_consequence` TEXT,
+  `af_kaviar` REAL,
+  `af_gnomad_genomes` REAL,
+  `af_jirdc` REAL,
+  `af_tommo` REAL,
+  `af_krgdb` REAL,
+  `af_converge` REAL,
+  `af_hgvd` REAL,
+  `gene_symbol` TEXT,
+  `hgvsc` TEXT,
+  `hgvsp` TEXT,
+  `dann` REAL,
+  `cadd_phred` REAL
+)
+"""
+
+class Variant(Base):
+    __tablename__ = 'variants'
+    variant_id = Column(Integer, primary_key = True)
+    chrom =  Column('stop', String(2))
+    pos = Column('position', String(255))
+
+
+
 
 
 meta.create_all()
