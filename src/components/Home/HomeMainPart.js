2b81c09f5acc833dab5814a8d884ac4116d4fbdb
Yuan Tian <tianyuan@cidatahub.com>
2019-11-11 00:10:04 +0000

    Now #51 is solved. The number would be updated after API calling finished.

--- src/components/Home/HomeMainPart.js
+++ src/components/Home/HomeMainPart.js
@@ -5,7 +5,6 @@ import compose from 'recompose/compose';
 
 import { withStyles } from '@material-ui/core/styles';
 import { withWidth, Grid, Box, Typography, Paper } from '@material-ui/core';
-import Loading from '../General/Loading';
 
 import {
   faTachometerAlt, faChartBar, faProjectDiagram, faUsers,
@@ -19,8 +18,8 @@ class HomeMainPart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      statistic: [],
-      loaded: false
+      exomes: "> 8,000",
+      variants: "> 4,000,000"
     };
   }
 
@@ -34,8 +33,8 @@ class HomeMainPart extends React.Component {
         let respond = res.data;
         console.log(respond);
         self.setState({
-          statistic: respond,
-          loaded: true
+          exomes: respond.exomes,
+          variants: respond.total_variants
         });
       })
       .catch(err => {
@@ -47,128 +46,124 @@ class HomeMainPart extends React.Component {
     const { classes } = this.props;
     const { t } = this.props;
 
-    const exomes = this.state.statistic.exomes
-    const variants = this.state.statistic.total_variants
+    const exomes = this.state.exomes
+    const variants = this.state.variants
 
-    if (this.state.loaded) {
-      return (
-        <div>
-          <Grid container className={classes.root}>
-            <Grid container justify='center'>
-              <Grid item xs={12} md={3} className={classes.gridpaper}>
-                <Paper elevation={0} className={classes.paper}>
-                  <FontAwesomeIcon
-                    icon={faTachometerAlt}
-                    color='#2E84CF'
-                    className={classes.fontawesomeicon}
-                  />
-                  <Typography component='div'>
-                    <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                      {t('HomePage.HomeMainPart.tab1.title')}
-                    </Box>
-                    <Box fontWeight='fontWeightLight' m={1}>
-                      {t('HomePage.HomeMainPart.tab1.description')}
-                    </Box>
-                  </Typography>
-                </Paper>
-              </Grid>
+    return (
+      <div>
+        <Grid container className={classes.root}>
+          <Grid container justify='center'>
+            <Grid item xs={12} md={3} className={classes.gridpaper}>
+              <Paper elevation={0} className={classes.paper}>
+                <FontAwesomeIcon
+                  icon={faTachometerAlt}
+                  color='#2E84CF'
+                  className={classes.fontawesomeicon}
+                />
+                <Typography component='div'>
+                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                    {t('HomePage.HomeMainPart.tab1.title')}
+                  </Box>
+                  <Box fontWeight='fontWeightLight' m={1}>
+                    {t('HomePage.HomeMainPart.tab1.description')}
+                  </Box>
+                </Typography>
+              </Paper>
+            </Grid>
 
-              <Grid item xs={12} md={3} className={classes.gridpaper}>
-                <Paper elevation={0} className={classes.paper}>
-                  <FontAwesomeIcon
-                    icon={faChartBar}
-                    color='#2E84CF'
-                    className={classes.fontawesomeicon}
-                  />
-                  <Typography component='div'>
-                    <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                      {t('HomePage.HomeMainPart.tab2.title')}
-                    </Box>
-                    <Box fontWeight='fontWeightLight' m={1}>
-                      {t('HomePage.HomeMainPart.tab2.description')}
-                    </Box>
-                  </Typography>
-                </Paper>
-              </Grid>
+            <Grid item xs={12} md={3} className={classes.gridpaper}>
+              <Paper elevation={0} className={classes.paper}>
+                <FontAwesomeIcon
+                  icon={faChartBar}
+                  color='#2E84CF'
+                  className={classes.fontawesomeicon}
+                />
+                <Typography component='div'>
+                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                    {t('HomePage.HomeMainPart.tab2.title')}
+                  </Box>
+                  <Box fontWeight='fontWeightLight' m={1}>
+                    {t('HomePage.HomeMainPart.tab2.description')}
+                  </Box>
+                </Typography>
+              </Paper>
+            </Grid>
 
-              <Grid item xs={12} md={3} className={classes.gridpaper}>
-                <Paper elevation={0} className={classes.paper}>
-                  <FontAwesomeIcon
-                    icon={faProjectDiagram}
-                    color='#2E84CF'
-                    className={classes.fontawesomeicon}
-                  />
-                  <Typography component='div'>
-                    <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                      {t('HomePage.HomeMainPart.tab3.title')}
-                    </Box>
-                    <Box fontWeight='fontWeightLight' m={1}>
-                      {t('HomePage.HomeMainPart.tab3.description')}
-                    </Box>
-                  </Typography>
-                </Paper>
-              </Grid>
+            <Grid item xs={12} md={3} className={classes.gridpaper}>
+              <Paper elevation={0} className={classes.paper}>
+                <FontAwesomeIcon
+                  icon={faProjectDiagram}
+                  color='#2E84CF'
+                  className={classes.fontawesomeicon}
+                />
+                <Typography component='div'>
+                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                    {t('HomePage.HomeMainPart.tab3.title')}
+                  </Box>
+                  <Box fontWeight='fontWeightLight' m={1}>
+                    {t('HomePage.HomeMainPart.tab3.description')}
+                  </Box>
+                </Typography>
+              </Paper>
             </Grid>
           </Grid>
+        </Grid>
 
-          <Grid container className={classes.root2}>
-            <Grid container justify='center'>
-              <Grid item xs={12} md={8} className={classes.gridpaper2}>
-                <Paper elevation={0} className={classes.paper2}>
-                  <FontAwesomeIcon
-                    icon={faUsers}
-                    color='#2E84CF'
-                    className={classes.fontawesomeicon}
-                  />
-                  <Typography component='div'>
-                    <Box fontWeight='fontWeightBold' fontSize='h4.fontSize' m={1}>
-                      {t('HomePage.HomeMainPart.statistic.title')}
-                    </Box>
-                    <Box
-                      fontWeight='fontWeightLight'
-                      fontSize='h6.fontSize'
-                      m={1}>
-                      <Trans i18nKey="HomePage.HomeMainPart.statistic.description" >
-                        Phenopolis includes data from {{ exomes }} exomes representing a total number of {{ variants }} variants.
+        <Grid container className={classes.root2}>
+          <Grid container justify='center'>
+            <Grid item xs={12} md={8} className={classes.gridpaper2}>
+              <Paper elevation={0} className={classes.paper2}>
+                <FontAwesomeIcon
+                  icon={faUsers}
+                  color='#2E84CF'
+                  className={classes.fontawesomeicon}
+                />
+                <Typography component='div'>
+                  <Box fontWeight='fontWeightBold' fontSize='h4.fontSize' m={1}>
+                    {t('HomePage.HomeMainPart.statistic.title')}
+                  </Box>
+                  <Box
+                    fontWeight='fontWeightLight'
+                    fontSize='h6.fontSize'
+                    m={1}>
+                    <Trans i18nKey="HomePage.HomeMainPart.statistic.description" >
+                      Phenopolis includes data from {{ exomes }} exomes representing a total number of {{ variants }} variants.
                     </Trans>
-                    </Box>
-                  </Typography>
-                </Paper>
-              </Grid>
+                  </Box>
+                </Typography>
+              </Paper>
             </Grid>
           </Grid>
+        </Grid>
 
-          <Grid container className={classes.root}>
-            <Grid container justify='center'>
-              <Grid item xs={12} md={8} className={classes.gridpaper}>
-                <Paper elevation={0} className={classes.paper}>
-                  <FontAwesomeIcon
-                    icon={faEnvelopeOpen}
-                    color='#2E84CF'
-                    className={classes.fontawesomeicon}
-                  />
-                  <Typography component='div'>
-                    <Box fontWeight='fontWeightBold' fontSize='h4.fontSize' m={1}>
-                      {t('HomePage.HomeMainPart.contact.title')}
-                    </Box>
-                    <Box
-                      fontWeight='fontWeightLight'
-                      fontSize='h6.fontSize'
-                      m={1}>
-                      <Trans i18nKey="HomePage.HomeMainPart.contact.description">
-                        Please feel free to contact us <a href='mailto:info@phenopolis.org' className={classes.a}>here</a> to give us feedback or report any issues.
+        <Grid container className={classes.root}>
+          <Grid container justify='center'>
+            <Grid item xs={12} md={8} className={classes.gridpaper}>
+              <Paper elevation={0} className={classes.paper}>
+                <FontAwesomeIcon
+                  icon={faEnvelopeOpen}
+                  color='#2E84CF'
+                  className={classes.fontawesomeicon}
+                />
+                <Typography component='div'>
+                  <Box fontWeight='fontWeightBold' fontSize='h4.fontSize' m={1}>
+                    {t('HomePage.HomeMainPart.contact.title')}
+                  </Box>
+                  <Box
+                    fontWeight='fontWeightLight'
+                    fontSize='h6.fontSize'
+                    m={1}>
+                    <Trans i18nKey="HomePage.HomeMainPart.contact.description">
+                      Please feel free to contact us <a href='mailto:info@phenopolis.org' className={classes.a}>here</a> to give us feedback or report any issues.
                     </Trans>
-                    </Box>
-                  </Typography>
-                </Paper>
-              </Grid>
+                  </Box>
+                </Typography>
+              </Paper>
             </Grid>
           </Grid>
-        </div>
-      );
-    } else {
-      return <Loading message={t('HomePage.HomeMainPart.message')} />;
-    }
+        </Grid>
+      </div>
+    )
   }
 }
 
