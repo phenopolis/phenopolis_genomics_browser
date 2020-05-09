1fd288c32d93c5d04707eab833536bb284cc1bb3
YuanTian1991 <tianyuan1991hit@gmail.com>
2020-05-09 02:28:03 +0100

    Filter front-end nearly done.

--- src/components/Table/VirtualTableFilter.js
+++ src/components/Table/VirtualTableFilter.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardActionArea, CardActions, CardContent, CardMedia, Button, Typography, Tabs, Tab, Grid, Paper, TextField, IconButton, Icon, List, ListItem } from '@material-ui/core';
+import { Card, CardContent, Button, Typography, Grid, TextField, IconButton, Icon, List, ListItem, Container, Menu, MenuItem, ListItemIcon } from '@material-ui/core';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import clsx from 'clsx';
+
+import Include from '../../assets/svg/include.svg'
+import Exclude from '../../assets/svg/exclude.svg'
 
 
 const top100Films = [
@@ -18,10 +21,65 @@ export default function MediaCard() {
   const classes = useStyles();
 
   const [value, setValue] = React.useState(0);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [myfilter, setFilter] = React.useState([]);
+  const operationOptions = [
+    { des: 'Equal', icon: '=' },
+    { des: 'Greater', icon: '>' },
+    { des: 'Smaller', icon: '<' },
+    { des: 'Include', icon: '⊂' },
+    { des: 'Exclude', icon: '⊄' }
+  ]
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const handleAddNewFilter = () => {
+    setFilter([...myfilter,
+    {
+      column: null,
+      operation: '=',
+      value: '',
+      andor: 'and'
+    }
+    ])
+  }
+
+  const handleOperationOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleOperationClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOperationChange = (index, operation) => {
+    window.alert(index)
+    const newFilter = [...myfilter];
+    newFilter[index].operation = operation
+    setFilter(newFilter)
+    handleOperationClose()
+  }
+
+  const handleSelectColumn = () => {
+  }
+
+  const handleAndOrChange = (index) => {
+    const newFilter = [...myfilter];
+    if (newFilter[index].andor === 'and') {
+      newFilter[index].andor = 'or'
+    } else {
+      newFilter[index].andor = 'and'
+    }
+    setFilter(newFilter)
+  }
+
+  const handleDeleteFilter = (index) => {
+    const newFilter = [...myfilter];
+    newFilter.splice(index, 1);
+    setFilter(newFilter)
+  }
 
   return (
     <Card className={classes.root}>
@@ -42,19 +100,22 @@ export default function MediaCard() {
         </div>
 
       </Card>
-      <CardContent>
 
+      <CardContent>
         <List>
           {
-            [1, 2, 3, 4, 5].map((item, index) => {
+            myfilter.map((item, index) => {
               return (
-                <ListItem style={{ border: '1px solid red', paddingTop: 0, paddingBottom: 0 }}>
-                  <Grid container spacing={0} style={{ border: '1px solid red', padding: 0, margin: 0 }}>
+                <ListItem key={index} style={{ paddingTop: 0, paddingBottom: 0 }}>
+                  <Grid container spacing={0} style={{ padding: 0, margin: 0 }} className={classes.filterList}>
                     <Grid item xs={1} className={classes.centerGrid}>
-                      <span>1</span>
+                      <span> {index + 1} </span>
                     </Grid>
+
                     <Grid item xs={4} className={classes.centerGrid}>
                       <Autocomplete
+                        value={item.column}
+                        onChange={handleSelectColumn}
                         id="combo-box-demo"
                         size="small"
                         options={top100Films}
@@ -62,40 +123,120 @@ export default function MediaCard() {
                         renderInput={(params) => <TextField {...params} label="Select Column" variant="outlined" />}
                       />
                     </Grid>
+
                     <Grid item xs={1} className={classes.centerGrid}>
                       <IconButton
                         edge="start"
                         size="small"
                         className={classes.menuButton}
                         color="inherit"
+                        onClick={handleOperationOpen}
                       >
-                        <Icon className={clsx(classes.iconHover, 'fas fa-equals')} />
+                        {item.operation}
+                      </IconButton>
+
+                      <Menu
+                        id="simple-menu"
+                        anchorEl={anchorEl}
+                        keepMounted
+                        open={Boolean(anchorEl)}
+                        onClose={handleOperationClose}
+                      >
+                        {
+                          operationOptions.map(function (operationItem, operationIndex, index) {
+                            return (
+                              <MenuItem key={operationIndex} onClick={() => handleOperationChange(index, operationItem.icon)}>
+                                <ListItemIcon>
+                                  <Typography variant="h5" noWrap>
+                                    {operationItem.icon}
+                                  </Typography>
+                                </ListItemIcon>
+                                <Typography variant="body2" noWrap>
+                                  {operationItem.des}
+                                </Typography>
+                              </MenuItem>
+                            )
+
+                          })
+                        }
+                        {/* {
+                          operationOptions.map((operationItem, operationIndex) => {
+                            return (
+                              <MenuItem key={operationIndex} onClick={() => handleOperationChange(index, operationItem.icon)}>
+                                <ListItemIcon>
+                                  <Typography variant="h5" noWrap>
+                                    {operationItem.icon}
+                                  </Typography>
+                                </ListItemIcon>
+                                <Typography variant="body2" noWrap>
+                                  {operationItem.des}
+                                </Typography>
+                              </MenuItem>
+                            )
+                          })
+                        } */}
+
+                      </Menu>
+                    </Grid>
+
+                    <Grid item xs={3} className={classes.centerGrid}>
+                      <TextField label="Value" variant="outlined" id="standard-size-small" size="small" />
+                    </Grid>
+
+                    <Grid item xs={2} className={classes.centerGrid}>
+                      <Button
+                        size="small"
+                        variant="contained"
+                        color={item.andor === "and" ? 'secondary' : 'primary'}
+                        className={classes.andorButton}
+                        onClick={() => handleAndOrChange(index)}>
+                        {item.andor}
+                      </Button>
+                    </Grid>
+
+                    <Grid item xs={1} className={classes.centerGrid}>
+                      <IconButton
+                        edge="start"
+                        size="small"
+                        color="inherit"
+                        onClick={() => handleDeleteFilter(index)}
+                      >
+                        <Icon className={clsx(classes.smallFilter, 'far fa-trash-alt')} />
                       </IconButton>
                     </Grid>
-                    <Grid item xs={4} className={classes.centerGrid}>
-                      <TextField label="Assign Value" variant="outlined" id="standard-size-small" size="small" />
-                    </Grid>
-                    <Grid item xs={2} className={classes.centerGrid}>
-                      <Button variant="contained" color="secondary" className={classes.andorButton}>
-                        AND
-                    </Button>
-                    </Grid>
+
                   </Grid>
                 </ListItem>
               )
             })
           }
         </List>
+
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Button onClick={handleAddNewFilter}>Add New Filter</Button>
+        </Grid>
       </CardContent>
 
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
+
+      <Container>
+        <Grid container
+          direction="row"
+          justify="flex-end"
+          alignItems="center">
+          <Button size="small" color="primary">
+            Apply
         </Button>
-        <Button size="small" color="primary">
-          Learn More
+          <Button size="small" color="secondary">
+            Cancel
         </Button>
-      </CardActions>
+        </Grid>
+      </Container>
+
     </Card >
   );
 }
@@ -103,6 +244,8 @@ export default function MediaCard() {
 const useStyles = makeStyles((theme) => ({
   root: {
     width: 600,
+    maxHeight: 600,
+    overflowY: 'auto'
   },
   media: {
     height: 80,
@@ -121,10 +264,12 @@ const useStyles = makeStyles((theme) => ({
   centerGrid: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "center"
+    justifyContent: "center",
+    padding: theme.spacing(0.8)
   },
   menuButton: {
     color: '#2E84CF',
+    fontWeight: 900,
     '&:hover': {
       cursor: 'pointer',
       color: 'black',
@@ -134,7 +279,24 @@ const useStyles = makeStyles((theme) => ({
     fontSize: 20,
     margin: theme.spacing(0),
   },
+  smallFilter: {
+    fontSize: 15,
+    margin: theme.spacing(0),
+  },
   andorButton: {
-    margin: theme.spacing(3),
+    marginRight: theme.spacing(3)
+  },
+  filterList: {
+    border: "1px solid white",
+    '&:hover': {
+      borderTop: "1px solid lightgrey",
+      borderBottom: "1px solid lightgrey",
+      fontWeight: 900
+    }
+  },
+  imageIcon: {
+    height: '0.8em',
+    width: '0.8em',
+    color: 'red'
   }
 }));
