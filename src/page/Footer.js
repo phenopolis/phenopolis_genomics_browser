cde78725435c24043c7ce426a2e19238c9a76d52
YuanTian1991 <tianyuan1991hit@gmail.com>
2020-06-15 15:41:39 +0100

    Fixed fontAwesome icon problem.

--- src/page/Footer.js
+++ src/page/Footer.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
 import { loadCSS } from 'fg-loadcss';
 import { Grid, Box, Container, CssBaseline, Typography, Icon } from '@material-ui/core'
-
-import { withRouter } from "react-router";
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithubSquare, faTwitterSquare } from '@fortawesome/free-brands-svg-icons'
-
-import compose from 'recompose/compose';
-import { useTranslation, Trans, withTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import i18next from "i18next";
 
 function Copyright() {
@@ -25,100 +18,104 @@ function Copyright() {
   );
 }
 
-class StickyFooter extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+export default function StickyFooter() {
+  const classes = useStyles();
+  const location = useLocation();
 
-  render() {
-    const { classes } = this.props;
-    const { t, i18n } = this.props;
-    const { match, location, history } = this.props;
+  React.useEffect(() => {
+    // window.alert(location.pathname)
+  }, [location.pathname]);
 
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        {location.pathname === '/' ? (
-          <Container component="main" className={classes.main} maxWidth="xl" >
-            <Grid container direction='row' justify='center' alignItems='center'>
-              <Grid item xs={12} md={6}>
-                <Typography component='div'>
-                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                    {t("Footer.About_Us")}
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    {i18next.t("Footer.Description")}
-                  </Box>
-                </Typography>
-              </Grid>
-              <Grid item xs={6} md={2}>
-                <Typography component='div'>
-                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                    {i18next.t("Footer.Useful_Links")}
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1} component={Link} to='/publications' className={classes.a}>
-                    {i18next.t("Footer.Publications")}
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="https://github.com/phenopolis" className={classes.a}>Github</a>
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="mailto:info@phenopolis.org" className={classes.a}> {i18next.t("Contact")} </a>
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="https://phenopolis.co.uk/" className={classes.a}>Phenopolis</a>
-                  </Box>
-                </Typography>
-              </Grid>
-              <Grid item xs={6} md={2}>
-                <Typography component='div'>
-                  <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
-                    {i18next.t("Footer.External_Links")}
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="http://human-phenotype-ontology.github.io/" className={classes.a}>Human Phenotype Ontology</a>
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="https://monarchinitiative.org/" className={classes.a}>Monarch Initiative</a>
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="https://phenotips.org/" className={classes.a}>Phenotips</a>
-                  </Box>
-                  <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
-                    <a href="https://decipher.sanger.ac.uk/" className={classes.a}>DECIPHER</a>
-                  </Box>
-                </Typography>
-              </Grid>
-            </Grid>
-          </Container>
-        ) : (null)}
 
-        <footer className={classes.footer}>
-          <Container component="main" className={classes.main} maxWidth="xl" >
-            <Grid container direction='row' justify='center' alignItems='center'>
-              <Grid item xs={12} md={8}>
-                <Typography variant="body2" gutterBottom>
-                  <Trans i18nKey="Footer.Citation">
-                    If you use Phenopolis, please cite us as: <a href='https://doi.org/10.1093/bioinformatics/btx147' className={classes.a}>Pontikos, N. et al. (2017).
-                    Phenopolis: an open platform for harmonization and analysis of genetic
-                and phenotypic data. Bioinformatics, 9, 7</a>
-                  </Trans>
-                </Typography>
-                <Copyright />
-              </Grid>
-              <Grid item xs={12} md={2}>
-                <a className={classes.a} href="https://twitter.com/phenopolis"><FontAwesomeIcon className={classes.iconHover} icon={faTwitterSquare} /></a>
-                <a className={classes.a} href="https://github.com/phenopolis/phenopolis"><FontAwesomeIcon className={classes.iconHover} icon={faGithubSquare} /></a>
-              </Grid>
-            </Grid>
-          </Container>
-        </footer>
-      </div>
+  React.useEffect(() => {
+    loadCSS(
+      'https://use.fontawesome.com/releases/v5.1.0/css/all.css',
+      document.querySelector('#font-awesome-css'),
     );
-  }
+  }, []);
+
+  const { t, i18n } = useTranslation();
+
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      {location.pathname === "/" ? (
+        <Container component="main" className={classes.main} maxWidth="xl" >
+          <Grid container direction='row' justify='center' alignItems='center'>
+            <Grid item xs={12} md={6}>
+              <Typography component='div'>
+                <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                  {t("Footer.About_Us")}
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  {i18next.t("Footer.Description")}
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={2}>
+              <Typography component='div'>
+                <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                  {i18next.t("Footer.Useful_Links")}
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1} component={Link} to='/publications' className={classes.a}>
+                  {i18next.t("Footer.Publications")}
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="https://github.com/phenopolis" className={classes.a}>Github</a>
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="mailto:info@phenopolis.org" className={classes.a}> {i18next.t("Contact")} </a>
+                </Box>
+              </Typography>
+            </Grid>
+            <Grid item xs={6} md={2}>
+              <Typography component='div'>
+                <Box fontWeight='fontWeightBold' fontSize='h5.fontSize' m={1}>
+                  {i18next.t("Footer.External_Links")}
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="http://human-phenotype-ontology.github.io/" className={classes.a}>Human Phenotype Ontology</a>
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="https://monarchinitiative.org/" className={classes.a}>Monarch Initiative</a>
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="https://phenotips.org/" className={classes.a}>Phenotips</a>
+                </Box>
+                <Box fontWeight='fontWeightRegular' fontSize='body2.fontSize' m={1}>
+                  <a href="https://decipher.sanger.ac.uk/" className={classes.a}>DECIPHER</a>
+                </Box>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Container>
+      ) : (null)}
+
+      <footer className={classes.footer}>
+        <Container component="main" className={classes.main} maxWidth="xl" >
+          <Grid container direction='row' justify='center' alignItems='center'>
+            <Grid item xs={12} md={8}>
+              <Typography variant="body2" gutterBottom>
+                <Trans i18nKey="Footer.Citation">
+                  If you use Phenopolis, please cite us as: <a href='https://doi.org/10.1093/bioinformatics/btx147' className={classes.a}>Pontikos, N. et al. (2017).
+                  Phenopolis: an open platform for harmonization and analysis of genetic
+                and phenotypic data. Bioinformatics, 9, 7</a>
+                </Trans>
+              </Typography>
+              <Copyright />
+            </Grid>
+            <Grid item xs={12} md={2}>
+              <a className={classes.a} href="https://twitter.com/phenopolis"><Icon className={clsx(classes.iconHover, 'fab fa-twitter-square')} /></a>
+              <a className={classes.a} href="https://github.com/phenopolis/phenopolis"><Icon className={clsx(classes.iconHover, 'fab fa-github-square')} /></a>
+            </Grid>
+          </Grid>
+        </Container>
+      </footer>
+    </div>
+  );
 }
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
@@ -148,17 +145,9 @@ const styles = theme => ({
     textAlign: 'left'
   },
   iconHover: {
-    color: 'white',
-    fontSize: '2em',
     margin: theme.spacing(1),
     '&:hover': {
       cursor: 'pointer'
     },
   }
-});
-
-StickyFooter.propTypes = {
-  classes: PropTypes.object.isRequired
-};
-
-export default withRouter(compose(withStyles(styles), withTranslation())(StickyFooter))
+}));
