c58d83a70341f26518d82edc38acee327bcf17c7
YuanTian1991 <tianyuan1991hit@gmail.com>
2020-06-19 02:03:34 +0100

    Set Home Title back.

--- src/page/Home.js
+++ src/page/Home.js
@@ -8,7 +8,7 @@ class Home extends React.Component {
     return (
       <React.Fragment>
         <CssBaseline />
-        <HomeBanner BannerText='Phenopolis Dev Version' />
+        <HomeBanner BannerText='Phenopolis' />
         <HomeMainPart />
       </React.Fragment>
     );
