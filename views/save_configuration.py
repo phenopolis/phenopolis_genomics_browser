e1c5c265a08f97518d7608561f5f0c1bd719d9e7
Nikolas Pontikos <n.pontikos@gmail.com>
2020-07-12 20:07:55 +0100

    Update save_configuration.py

--- views/save_configuration.py
+++ views/save_configuration.py
@@ -1,6 +1,6 @@
 from views import *
 from views.auth import requires_auth
-from views.postgres import postgres_cursor
+from views.postgres import postgres_cursor, get_db
 
 @application.route('/<language>/save_configuration/<pageType>/<pagePart>', methods=['POST'])
 @application.route('/save_configuration/<pageType>/<pagePart>', methods=['POST'])
