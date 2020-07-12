9c636e507fbd2d45f565e96724d883b1fa986d61
priesgof <priesgoferreiro@gmail.com>
2020-07-12 18:53:07 +0200

    make sure log config works on gunicorn + clean the house a bit + integrate ujson instead of native json

--- application.py
+++ application.py
@@ -1,47 +1,8 @@
 from views import application
-import logging
-from logging.config import dictConfig
-from flask.logging import default_handler
 
 '''
 Flask app
 '''
-
-
-def _configure_logs():
-    dictConfig({
-        'version': 1,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s'
-            }
-        },
-        'handlers': {
-            'wsgi': {
-                'class': 'logging.StreamHandler',
-                'stream': 'ext://flask.logging.wsgi_errors_stream',
-                'formatter': 'default'
-            },
-            'info_rotating_file_handler': {
-                'level': 'INFO',
-                'formatter': 'default',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'filename': 'phenopolis.log',
-                'mode': 'a',
-                'maxBytes': 1048576,
-                'backupCount': 10
-            }
-        },
-        'root': {
-            'level': 'INFO',
-            'handlers': ['wsgi']
-        }
-    })
-    # add SQLalchemy logs
-    logging.getLogger('sqlalchemy').addHandler(default_handler)
-
-
 if __name__ == "__main__":
-    _configure_logs()   # NOTE: this needs to happen before starting the application
     application.debug = True
     application.run()
--- views/__init__.py
+++ views/__init__.py
@@ -1,7 +1,7 @@
 '''
 Package to init views
 '''
-import json
+import ujson as json
 import os
 import re
 import itertools
@@ -21,48 +21,80 @@ from flask_mail import Mail, Message
 from passlib.hash import argon2
 from werkzeug.exceptions import HTTPException
 from flask_sqlalchemy import SQLAlchemy
+import logging
+from logging.config import dictConfig
+from flask.logging import default_handler
 import pysam
 from db import *
 
+
+def _configure_logs():
+    dictConfig({
+        'version': 1,
+        'formatters': {
+            'default': {
+                'format': '%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s'
+            }
+        },
+        'handlers': {
+            'wsgi': {
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://flask.logging.wsgi_errors_stream',
+                'formatter': 'default'
+            },
+            'info_rotating_file_handler': {
+                'level': 'INFO',
+                'formatter': 'default',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': 'phenopolis.log',
+                'mode': 'a',
+                'maxBytes': 1048576,
+                'backupCount': 10
+            }
+        },
+        'root': {
+            'level': 'INFO',
+            'handlers': ['wsgi']
+        }
+    })
+    # add SQLalchemy logs
+    logging.getLogger('sqlalchemy').addHandler(default_handler)
+
+
+def _load_config():
+    application.config['SERVED_URL'] = os.getenv('SERVED_URL', '127.0.0.1')
+    application.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
+    application.config['MAIL_PORT'] = os.getenv('MAIL_PORT', '587')
+    application.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'no-reply@phenopolis.org')
+    application.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'get_password')
+    application.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'true') == 'true'
+    application.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'false') == 'true'
+    application.config['MAIL_SUPPRESS_SEND'] = os.getenv('MAIL_SUPPRESS_SEND', 'true') == 'true'
+    application.config['DB_HOST'] = os.getenv('POSTGRES_HOST', '0.0.0.0')
+    application.config['DB_DATABASE'] = os.getenv('POSTGRES_DB', 'phenopolis_db')
+    application.config['DB_USER'] = os.getenv('POSTGRES_USER', 'phenopolis_api')
+    application.config['DB_PASSWORD'] = os.getenv('POSTGRES_PASSWORD', 'phenopolis_api')
+    application.config['DB_PORT'] = os.getenv('POSTGRES_PORT', '5432')
+    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db_uri = 'postgresql+psycopg2://%s:%s@%s/%s' % (application.config['DB_USER'],
+                                                    application.config['DB_PASSWORD'],
+                                                    application.config['DB_HOST'],
+                                                    application.config['DB_DATABASE'])
+    application.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+
+
+def _init_sqlalchemy():
+    database = SQLAlchemy(application)
+    database.init_app(application)
+    application.session_interface = SqlAlchemySessionInterface(application, database, "test_sessions", "test_sess_")
+    application.permanent_session_lifetime = datetime.timedelta(hours=1)
+
+
+_configure_logs()   # NOTE: this needs to happen before starting the application
 # Load default config and override config from an environment variable
 application = Flask(__name__)
-
-mail_use_tls = os.getenv('MAIL_USE_TLS', 'true')
-mail_use_ssl = os.getenv('MAIL_USE_SSL', 'false')
-mail_suppress_send = os.getenv('MAIL_SUPPRESS_SEND', 'true')
-
-application.config['SERVED_URL'] = os.getenv('SERVED_URL', '127.0.0.1')
-
-application.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-application.config['MAIL_PORT'] = os.getenv('MAIL_PORT', '587')
-application.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'no-reply@phenopolis.org')
-application.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'get_password')
-application.config['MAIL_USE_TLS'] = mail_use_tls == 'true'
-application.config['MAIL_USE_SSL'] = mail_use_ssl == 'true'
-application.config['MAIL_SUPPRESS_SEND'] = mail_suppress_send == 'true'
-
-application.config['DB_HOST'] = os.getenv('POSTGRES_HOST', '0.0.0.0')
-application.config['DB_DATABASE'] = os.getenv('POSTGRES_DB', 'phenopolis_db')
-application.config['DB_USER'] = os.getenv('POSTGRES_USER', 'phenopolis_api')
-application.config['DB_PASSWORD'] = os.getenv('POSTGRES_PASSWORD', 'phenopolis_api')
-application.config['DB_PORT'] = os.getenv('POSTGRES_PORT', '5432')
-
-application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db_uri = 'postgresql+psycopg2://%s:%s@%s/%s' % (application.config['DB_USER'],
-                                                application.config['DB_PASSWORD'],
-                                                application.config['DB_HOST'],
-                                                application.config['DB_DATABASE'])
-application.config['SQLALCHEMY_DATABASE_URI'] = db_uri
-
-SESSION_COOKIE_NAME = 'phenopolis_api'
-SESSION_TYPE = 'sqlalchemy'
-SESSION_SQLALCHEMY = create_engine(application.config['SQLALCHEMY_DATABASE_URI'], echo = True)
-
-db = SQLAlchemy(application)
-db.init_app(application)
-
-application.session_interface = SqlAlchemySessionInterface(application, db, "test_sessions", "test_sess_")
-application.permanent_session_lifetime = datetime.timedelta(hours = 1)
+_load_config()
+_init_sqlalchemy()
 
 Compress(application)
 cache = Cache(application, config={'CACHE_TYPE': 'simple'})
