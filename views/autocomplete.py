04b68355513e3069726c3264ed946738698cc932
Alan Silva <alanwilter@gmail.com>
2020-07-20 22:21:01 +0200

    Pylinted, reorganised imported modules and improved on #53

--- views/autocomplete.py
+++ views/autocomplete.py
@@ -1,10 +1,12 @@
+'''
+Autocomplete view
+'''
 from logzero import logger
 
-from views import *
+from views import application, re, itertools, Response, session, jsonify, json, cursor2dict
 from views.exceptions import PhenopolisException
 from views.auth import requires_auth
 from views.postgres import postgres_cursor
-import re
 
 SEARCH_RESULTS_LIMIT = 20
 
@@ -15,7 +17,12 @@ SEARCH_RESULTS_LIMIT = 20
 @application.route('/autocomplete/<query>')
 @requires_auth
 def autocomplete(query, query_type=''):
-    logger.debug("Autocomplete query '{}' and query type '{}'".format(query, query_type))
+    '''
+    :param query:
+    :param query_type:
+    '''
+
+    logger.debug("Autocomplete query '%s' and query type '%s'", query, query_type)
 
     cursor = postgres_cursor()
     if query_type == 'gene':
@@ -76,28 +83,28 @@ def _search_genes(cursor, query):
 
 
 def _search_variants(cursor, query):
-    chrom, pos, ref, alt = _parse_variant_from_query(query)
+    chrom, pos, ref, alt = _parse_variant_from_query(query.upper())
     if chrom is not None and ref is not None and alt is not None:
         cursor.execute(
-            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"={pos} and "
+            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"::text like '{pos}%' and "
             "\"REF\"='{ref}' and \"ALT\"='{alt}' limit {limit}".
-                format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos, ref=ref, alt=alt))
+            format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos, ref=ref, alt=alt))
     elif chrom is not None and ref is not None and alt is None:
         cursor.execute(
-            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"={pos} and "
+            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"::text like '{pos}%' and "
             "\"REF\"='{ref}' limit {limit}".
-                format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos, ref=ref))
+            format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos, ref=ref))
     elif chrom is not None and ref is None:
         cursor.execute(
-            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"={pos} "
+            "select \"CHROM\", \"POS\", \"REF\", \"ALT\" from variants where \"CHROM\"='{chrom}' and \"POS\"::text like '{pos}%' "
             "limit {limit}".
-                format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos))
+            format(limit=SEARCH_RESULTS_LIMIT, chrom=chrom, pos=pos))
     else:
         # no variant pattern, we perform no search
         return []
     variant_hits = cursor2dict(cursor)
     # while the search is performed on the upper cased gene name, it returns the original gene name
-    return ["{chrom}-{pos}-{ref}-{alt}".format(chrom=x['CHROM'], pos=x['POS'], ref=x['REF'], alt=x['ALT']) 
+    return ["{chrom}-{pos}-{ref}-{alt}".format(chrom=x['CHROM'], pos=x['POS'], ref=x['REF'], alt=x['ALT'])
             for x in variant_hits]
 
 
@@ -109,13 +116,13 @@ def _parse_variant_from_query(query):
     It expects fields in the variant to be separated by -, : or >. This last one only for separation between reference
     and alternte.
     """
-    match = re.compile('^(\w+)[-:](\d+)[-:]([ACGT]+)[-:>]([ACGT]+)$').match(query)
+    match = re.compile(r'^(\w+)[-:](\d+)[-:]([ACGT]+)[-:>]([ACGT]+)$').match(query)
     if match:
         return match.group(1), match.group(2), match.group(3), match.group(4)
-    match = re.compile('^(\w+)[-:](\d+)[-:]([ACGT]+)$').match(query)
+    match = re.compile(r'^(\w+)[-:](\d+)[-:]([ACGT]+)$').match(query)
     if match:
         return match.group(1), match.group(2), match.group(3), None
-    match = re.compile('^(\w+)[-:](\d+)$').match(query)
+    match = re.compile(r'^(\w+)[-:](\d+)$').match(query)
     if match:
         return match.group(1), match.group(2), None, None
     return None, None, None, None
@@ -124,16 +131,18 @@ def _parse_variant_from_query(query):
 @application.route('/best_guess/<query>')
 @requires_auth
 def best_guess(query=''):
+    '''
+    :param query:
+    '''
     application.logger.debug(query)
     if query.startswith('gene:'):
         return jsonify(redirect='/gene/{}'.format(query.replace('gene:', '')))
-    elif query.startswith('patient:') or query.startswith('PH'):
+    if query.startswith('patient:') or query.startswith('PH'):
         return jsonify(redirect='/individual/{}'.format(query.replace('patient:', '')))
-    elif query.startswith('phenotype:'):
+    if query.startswith('phenotype:'):
         return jsonify(redirect='/hpo/{}'.format(query.replace('phenotype:', '')))
-    elif query.startswith('variant:'):
+    if query.startswith('variant:'):
         return jsonify(redirect='/variant/{}'.format(query.replace('variant:', '')))
-    # TODO: do we need this one yet? probably not.
-    elif '-' in query and len(query.split('-')) == 4:
+    if '-' in query and len(query.split('-')) == 4:
         return jsonify(redirect='/variant/{}'.format(query.replace('variant:', '')))
     return jsonify(message='Could not find search query'), 420
