7ee008a86c6b8e4a2b9d58642bdefa11a1f383ff
Alan <alanwilter@gmail.com>
2020-07-27 11:01:41 +0200

    Code tidy (#97)
    * Udated gitignore
    
    * Code tidied after Daniele's suggestions
    
    * Code tidied and improved autocomplete search
    
    * Introducing tox configuration
    
    * Flake8 and Black applied
    
    * Updated tox.ini and created pyproject.toml for Black code formatter
    
    * Files updated by Black new config in pyproject.toml
    
    * Refactored imports from db
    
    * To include Black and Flake8 for Tox
    
    * Commented out _gene_hpo_ids, not used

--- views/__init__.py
+++ views/__init__.py
@@ -1,7 +1,6 @@
-'''
+"""
 Package to init views
-'''
-import ujson as json
+"""
 import os
 import re
 import itertools
@@ -11,9 +10,10 @@ from functools import wraps
 from collections import defaultdict, Counter, OrderedDict
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.config import dictConfig
 from time import strftime
-import psycopg2
 from flask import Flask, session, current_app, g, Response, request, redirect, jsonify
+from flask.logging import default_handler
 from flask_sessionstore import Session, SqlAlchemySessionInterface
 from flask_compress import Compress
 from flask_caching import Cache
@@ -21,66 +21,63 @@ from flask_mail import Mail, Message
 from passlib.hash import argon2
 from werkzeug.exceptions import HTTPException
 from flask_sqlalchemy import SQLAlchemy
-import logging
-from logging.config import dictConfig
-from flask.logging import default_handler
+import psycopg2
+import ujson as json
 import pysam
-from db import *
 
 
 def _configure_logs():
-    dictConfig({
-        'version': 1,
-        'formatters': {
-            'default': {
-                'format': '%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s'
-            }
-        },
-        'handlers': {
-            'wsgi': {
-                'class': 'logging.StreamHandler',
-                'stream': 'ext://flask.logging.wsgi_errors_stream',
-                'formatter': 'default'
+    dictConfig(
+        {
+            "version": 1,
+            "formatters": {
+                "default": {"format": "%(asctime)s-%(levelname)s-%(name)s::%(module)s|%(lineno)s:: %(message)s"}
             },
-            'info_rotating_file_handler': {
-                'level': 'INFO',
-                'formatter': 'default',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'filename': 'phenopolis.log',
-                'mode': 'a',
-                'maxBytes': 1048576,
-                'backupCount': 10
-            }
-        },
-        'root': {
-            'level': 'INFO',
-            'handlers': ['wsgi']
+            "handlers": {
+                "wsgi": {
+                    "class": "logging.StreamHandler",
+                    "stream": "ext://flask.logging.wsgi_errors_stream",
+                    "formatter": "default",
+                },
+                "info_rotating_file_handler": {
+                    "level": "INFO",
+                    "formatter": "default",
+                    "class": "logging.handlers.RotatingFileHandler",
+                    "filename": "phenopolis.log",
+                    "mode": "a",
+                    "maxBytes": 1048576,
+                    "backupCount": 10,
+                },
+            },
+            "root": {"level": "INFO", "handlers": ["wsgi"]},
         }
-    })
+    )
     # add SQLalchemy logs
-    logging.getLogger('sqlalchemy').addHandler(default_handler)
+    logging.getLogger("sqlalchemy").addHandler(default_handler)
 
 
 def _load_config():
-    application.config['SERVED_URL'] = os.getenv('SERVED_URL', '127.0.0.1')
-    application.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-    application.config['MAIL_PORT'] = os.getenv('MAIL_PORT', '587')
-    application.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'no-reply@phenopolis.org')
-    application.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'get_password')
-    application.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS', 'true') == 'true'
-    application.config['MAIL_USE_SSL'] = os.getenv('MAIL_USE_SSL', 'false') == 'true'
-    application.config['MAIL_SUPPRESS_SEND'] = os.getenv('MAIL_SUPPRESS_SEND', 'true') == 'true'
-    application.config['DB_HOST'] = os.getenv('POSTGRES_HOST', '0.0.0.0')
-    application.config['DB_DATABASE'] = os.getenv('POSTGRES_DB', 'phenopolis_db')
-    application.config['DB_USER'] = os.getenv('POSTGRES_USER', 'phenopolis_api')
-    application.config['DB_PASSWORD'] = os.getenv('POSTGRES_PASSWORD', 'phenopolis_api')
-    application.config['DB_PORT'] = os.getenv('POSTGRES_PORT', '5432')
-    application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    db_uri = 'postgresql+psycopg2://%s:%s@%s/%s' % (application.config['DB_USER'],
-                                                    application.config['DB_PASSWORD'],
-                                                    application.config['DB_HOST'],
-                                                    application.config['DB_DATABASE'])
-    application.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    application.config["SERVED_URL"] = os.getenv("SERVED_URL", "127.0.0.1")
+    application.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER", "smtp.gmail.com")
+    application.config["MAIL_PORT"] = os.getenv("MAIL_PORT", "587")
+    application.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME", "no-reply@phenopolis.org")
+    application.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD", "get_password")
+    application.config["MAIL_USE_TLS"] = os.getenv("MAIL_USE_TLS", "true") == "true"
+    application.config["MAIL_USE_SSL"] = os.getenv("MAIL_USE_SSL", "false") == "true"
+    application.config["MAIL_SUPPRESS_SEND"] = os.getenv("MAIL_SUPPRESS_SEND", "true") == "true"
+    application.config["DB_HOST"] = os.getenv("POSTGRES_HOST", "0.0.0.0")
+    application.config["DB_DATABASE"] = os.getenv("POSTGRES_DB", "phenopolis_db")
+    application.config["DB_USER"] = os.getenv("POSTGRES_USER", "phenopolis_api")
+    application.config["DB_PASSWORD"] = os.getenv("POSTGRES_PASSWORD", "phenopolis_api")
+    application.config["DB_PORT"] = os.getenv("POSTGRES_PORT", "5432")
+    application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+    db_uri = "postgresql+psycopg2://%s:%s@%s/%s" % (
+        application.config["DB_USER"],
+        application.config["DB_PASSWORD"],
+        application.config["DB_HOST"],
+        application.config["DB_DATABASE"],
+    )
+    application.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 
 
 def _init_sqlalchemy():
@@ -95,26 +92,26 @@ def cursor2dict(cursor):
     return [dict(zip(headers, r)) for r in cursor.fetchall()]
 
 
-_configure_logs()   # NOTE: this needs to happen before starting the application
+_configure_logs()  # NOTE: this needs to happen before starting the application
 # Load default config and override config from an environment variable
 application = Flask(__name__)
 _load_config()
 _init_sqlalchemy()
 
 Compress(application)
-cache = Cache(application, config={'CACHE_TYPE': 'simple'})
+cache = Cache(application, config={"CACHE_TYPE": "simple"})
 mail = Mail(application)
 
 # These imports must be placed at the end of this file
-# pylint: disable=wrong-import-position
-import views.general  # @IgnorePep8
-import views.postgres  # @IgnorePep8
-import views.auth  # @IgnorePep8
-import views.statistics  # @IgnorePep8
-import views.gene  # @IgnorePep8
-import views.variant  # @IgnorePep8
-import views.individual  # @IgnorePep8
-import views.hpo  # @IgnorePep8
-import views.users  # @IgnorePep8
-import views.autocomplete  # @IgnorePep8
-import views.save_configuration  # @IgnorePep8
+# flake8: noqa E402
+import views.general
+import views.postgres
+import views.auth
+import views.statistics
+import views.gene
+import views.variant
+import views.individual
+import views.hpo
+import views.users
+import views.autocomplete
+import views.save_configuration
--- views/variant.py
+++ views/variant.py
@@ -1,81 +1,105 @@
-'''
+"""
 variant view
-'''
+"""
 import os
 import boto3
 import requests
-from views import application, pysam, json, session, and_, Variant, cursor2dict
+import pysam
+from views import application, json, session, cursor2dict
 from views.auth import requires_auth
 from views.postgres import postgres_cursor, get_db_session
 from views.general import process_for_display
+from db import Variant
+from sqlalchemy import and_
 
-@application.route('/<language>/variant/<variant_id>')
-@application.route('/<language>/variant/<variant_id>/<subset>')
-@application.route('/variant/<variant_id>')
-@application.route('/variant/<variant_id>/<subset>')
+
+@application.route("/<language>/variant/<variant_id>")
+@application.route("/<language>/variant/<variant_id>/<subset>")
+@application.route("/variant/<variant_id>")
+@application.route("/variant/<variant_id>/<subset>")
 @requires_auth
-def variant(variant_id, subset='all', language='en'):
-    '''
-    :param variant_id:
-    :param subset:
-    :param language:
-    '''
-
+def variant(variant_id, subset="all", language="en"):
     c = postgres_cursor()
     c.execute("select external_id, internal_id from individuals")
     pheno_ids = cursor2dict(c)
-    phenoid_mapping = {ind['external_id']: ind['internal_id'] for ind in pheno_ids}
+    phenoid_mapping = {ind["external_id"]: ind["internal_id"] for ind in pheno_ids}
     # print(phenoid_mapping)
-    chrom, pos, ref, alt, = variant_id.split('-')
-    url = 'https://myvariant.info/v1/variant/chr%s:g.%s%s>%s?fields=clinvar.rcv.clinical_significance&dotfield=true' % (chrom, pos, ref, alt,)
+    chrom, pos, ref, alt, = variant_id.split("-")
+    url = "https://myvariant.info/v1/variant/chr%s:g.%s%s>%s?fields=clinvar.rcv.clinical_significance&dotfield=true" % (
+        chrom,
+        pos,
+        ref,
+        alt,
+    )
     x = requests.get(url).json()
     if x:
-        clinical_significance = str(x.get("clinvar.rcv.clinical_significance", ''))
-    else:
-        clinical_significance = ''
+        clinical_significance = str(x.get("clinvar.rcv.clinical_significance", ""))
     pos = int(pos)
-    s3 = boto3.client('s3', aws_secret_access_key=os.environ['VCF_S3_SECRET'],
-                      aws_access_key_id=os.environ['VCF_S3_KEY'],
-                      region_name="eu-west-2",
-                      config=boto3.session.Config(signature_version='s3v4'))
-    vcf_index = s3.generate_presigned_url('get_object', Params={'Bucket': 'phenopolis-vcf', 'Key': 'August2019/merged2.vcf.gz.tbi'}, ExpiresIn=5000)
-    vcf_file = s3.generate_presigned_url('get_object', Params={'Bucket': 'phenopolis-vcf', 'Key': 'August2019/merged2.vcf.gz'}, ExpiresIn=5000)
+    s3 = boto3.client(
+        "s3",
+        aws_secret_access_key=os.environ["VCF_S3_SECRET"],
+        aws_access_key_id=os.environ["VCF_S3_KEY"],
+        region_name="eu-west-2",
+        config=boto3.session.Config(signature_version="s3v4"),
+    )
+    vcf_index = s3.generate_presigned_url(
+        "get_object", Params={"Bucket": "phenopolis-vcf", "Key": "August2019/merged2.vcf.gz.tbi"}, ExpiresIn=5000,
+    )
+    vcf_file = s3.generate_presigned_url(
+        "get_object", Params={"Bucket": "phenopolis-vcf", "Key": "August2019/merged2.vcf.gz"}, ExpiresIn=5000,
+    )
     variant_file = pysam.VariantFile(vcf_file, index_filename=vcf_index)
     # samples = variant_file.header.samples
     variant_dict = dict()
-    for v in variant_file.fetch(chrom, pos - 1, pos):
-        variant_dict['pos'] = v.pos
-        variant_dict['start'] = v.start
-        variant_dict['stop'] = v.stop
-        variant_dict['ref'] = v.ref
-        variant_dict['alt'] = alt
-        variant_dict['alleles'] = v.alleles
-        variant_dict['alts'] = v.alts
-        variant_dict['rlen'] = v.rlen
-        variant_dict['chrom'] = v.chrom
-        variant_dict['id'] = v.id
-        variant_dict['rid'] = v.rid
-        variant_dict['qual'] = v.qual
-        variant_dict['filter'] = list(v.filter.keys())
-        variant_dict['format'] = {(v.format[k].name, v.format[k].id,) for k in v.format.keys()}
-        variant_dict['info'] = dict(v.info)
-        variant_dict['genotypes'] = [{'sample': [{'display': phenoid_mapping.get(s)}], 'GT': v.samples[s].get('GT', ''), 'AD': v.samples[s].get('AD', ''), 'DP':v.samples[s].get('DP', '')} for s in v.samples]
-    c.execute("select config from user_config u where u.user_name='%s' and u.language='%s' and u.page='%s' limit 1" % (session['user'], language, 'variant'))
+    v = next(variant_file.fetch(chrom, pos - 1, pos))
+    # for v in variant_file.fetch(chrom, pos - 1, pos):
+    #         variant_dict['pos'] = v.pos
+    #         variant_dict['start'] = v.start
+    #         variant_dict['stop'] = v.stop
+    #         variant_dict['ref'] = v.ref
+    #         variant_dict['alt'] = alt
+    #         variant_dict['alleles'] = v.alleles
+    #         variant_dict['alts'] = v.alts
+    #         variant_dict['rlen'] = v.rlen
+    #         variant_dict['chrom'] = v.chrom
+    #         variant_dict['id'] = v.id
+    #         variant_dict['rid'] = v.rid
+    #         variant_dict['qual'] = v.qual
+    #         variant_dict['filter'] = list(v.filter.keys())
+    #         variant_dict['format'] = {(v.format[k].name, v.format[k].id,) for k in v.format.keys()}
+    #         variant_dict['info'] = dict(v.info)
+    variant_dict["genotypes"] = [
+        {
+            "sample": [{"display": phenoid_mapping.get(s)}],
+            "GT": v.samples[s].get("GT", ""),
+            "AD": v.samples[s].get("AD", ""),
+            "DP": v.samples[s].get("DP", ""),
+        }
+        for s in v.samples
+    ]
+    c.execute(
+        "select config from user_config u where u.user_name='%s' and u.language='%s' and u.page='%s' limit 1"
+        % (session["user"], language, "variant")
+    )
     x = c.fetchone()[0]
     # CHROM, POS, REF, ALT, = variant_id.split('-')
-    data = get_db_session().query(Variant).filter(and_(Variant.CHROM == chrom, Variant.POS == pos, Variant.REF == ref, Variant.ALT == alt))
+    data = (
+        get_db_session()
+        .query(Variant)
+        .filter(and_(Variant.CHROM == chrom, Variant.POS == pos, Variant.REF == ref, Variant.ALT == alt,))
+    )
     var = [p.as_dict() for p in data]
     process_for_display(var)
     if len(var) == 0:
         var = {}
     else:
         var = var[0]
-    x[0]['metadata']['data'] = [var]
-    x[0]['individuals']['data'] = [var]
-    x[0]['frequency']['data'] = [var]
-    x[0]['consequence']['data'] = [var]
-    x[0]['genotypes']['data'] = variant_dict['genotypes']
-    x[0]['preview'] = [['Clinvar', clinical_significance]]
-    if subset == 'all':
+    x[0]["metadata"]["data"] = [var]
+    x[0]["individuals"]["data"] = [var]
+    x[0]["frequency"]["data"] = [var]
+    x[0]["consequence"]["data"] = [var]
+    x[0]["genotypes"]["data"] = variant_dict["genotypes"]
+    x[0]["preview"] = [["Clinvar", clinical_significance]]
+    if subset == "all":
         return json.dumps(x)
     return json.dumps([{subset: y[subset]} for y in x])
