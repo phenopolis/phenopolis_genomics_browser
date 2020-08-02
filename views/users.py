f2a7254e164469c01269d0b34afb440bf312c0c2
priesgof <priesgoferreiro@gmail.com>
2020-08-03 00:42:53 +0200

    add endpoint to post user_individuals

--- views/users.py
+++ views/users.py
@@ -4,10 +4,11 @@ Users view
 from flask import session, request, jsonify
 from passlib.handlers.argon2 import argon2
 
-from db import User
+from db import User, User_Individual, Individual
 from views import application
 from views.auth import requires_auth, check_auth
 from views.exceptions import PhenopolisException
+from views.helpers import _get_json_payload, _parse_payload
 from views.postgres import postgres_cursor, get_db_session
 
 
@@ -42,38 +43,37 @@ def create_user():
     if session["user"] != "Admin":
         return jsonify(error="You do not have permission to create new users"), 403
 
-    if not request.is_json:
-        return jsonify(success=False, error="Only mimetype application/json is accepted"), 400
-
-    payload = request.get_json(silent=True)
-    if payload is None:
-        return jsonify(success=False, error="Empty payload or wrong formatting"), 400
-    application.logger.debug(payload)
+    try:
+        payload = _get_json_payload()
+    except PhenopolisException as e:
+        return jsonify(success=False, error=str(e)), 400
 
     # parse the JSON data into an individual, non existing fields will trigger a TypeError
     try:
-        new_user = User(**payload)
+        new_users = _parse_payload(payload, User)
     except TypeError as e:
         application.logger.error(str(e))
         return jsonify(success=False, error=str(e)), 400
 
     # checks individuals validity
     try:
-        _check_user_valid(new_user)
+        for u in new_users:
+            _check_user_valid(u)
     except PhenopolisException as e:
         application.logger.error(str(e))
         return jsonify(success=False, error=str(e)), 400
 
     # encode password
-    new_user.argon_password = argon2.hash(new_user.argon_password)
+    for u in new_users:
+        u.argon_password = argon2.hash(u.argon_password)
 
     sqlalchemy_session = get_db_session()
     request_ok = True
-    message = "User was created"
-    user_id = new_user.user
+    message = "Users were created"
+    user_ids = ",".join([u.user for u in new_users])
     try:
-        # insert user
-        sqlalchemy_session.add(new_user)
+        # insert users
+        sqlalchemy_session.add_all(new_users)
         sqlalchemy_session.commit()
     except Exception as e:
         sqlalchemy_session.rollback()
@@ -86,7 +86,64 @@ def create_user():
     if not request_ok:
         return jsonify(success=False, message=message), 500
     else:
-        return jsonify(success=True, message=message, id=user_id), 200
+        return jsonify(success=True, message=message, id=user_ids), 200
+
+
+@application.route("/user-individual", methods=["POST"])
+@requires_auth
+def create_user_idividual():
+    if session["user"] != "Admin":
+        return jsonify(error="You do not have permission to give access to individuals to other users"), 403
+
+    try:
+        payload = _get_json_payload()
+    except PhenopolisException as e:
+        return jsonify(success=False, error=str(e)), 400
+
+    # parse the JSON data into a user_individual, non existing fields will trigger a TypeError
+    try:
+        new_user_individuals = _parse_payload(payload, User_Individual)
+    except TypeError as e:
+        application.logger.error(str(e))
+        return jsonify(success=False, error=str(e)), 400
+
+    # checks user individuals validity
+    try:
+        for u in new_user_individuals:
+            _check_user_individual_valid(u)
+    except PhenopolisException as e:
+        application.logger.error(str(e))
+        return jsonify(success=False, error=str(e)), 400
+
+    db_session = get_db_session()
+    request_ok = True
+    message = "User individuals were created"
+    try:
+        # insert user individuals
+        for u in new_user_individuals:
+            # TODO: should not all these checks happen at the DB?
+            if db_session.query(User.user).filter(User.user.match(u.user)).count() != 1:
+                raise PhenopolisException("Trying to add an entry in user_individual to a non existing user")
+            if db_session.query(Individual.internal_id).filter(Individual.internal_id.match(u.internal_id))\
+                    .count() != 1:
+                raise PhenopolisException("Trying to add an entry in user_individual to a non existing individual")
+            if db_session.query(User_Individual).filter(User_Individual.user.match(u.user)).filter(
+                                                        User_Individual.internal_id.match(u.internal_id)).count() > 0:
+                raise PhenopolisException("Trying to add an entry in user_individual that already exists")
+            db_session.add(u)
+        db_session.commit()
+    except Exception as e:
+        db_session.rollback()
+        application.logger.exception(e)
+        request_ok = False
+        message = str(e)
+    finally:
+        db_session.close()
+
+    if not request_ok:
+        return jsonify(success=False, message=message), 500
+    else:
+        return jsonify(success=True, message=message), 200
 
 
 def _check_user_valid(new_user: User):
@@ -96,3 +153,12 @@ def _check_user_valid(new_user: User):
         raise PhenopolisException("Missing user name")
     if new_user.argon_password is None or new_user.argon_password == "":
         raise PhenopolisException("Missing password")
+
+
+def _check_user_individual_valid(new_user_individual: User_Individual):
+    if new_user_individual is None:
+        raise PhenopolisException("Null user individual")
+    if new_user_individual.user is None or new_user_individual.user == "":
+        raise PhenopolisException("Missing user")
+    if new_user_individual.internal_id is None or new_user_individual.internal_id == "":
+        raise PhenopolisException("Missing individual id")
