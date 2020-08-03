bf1967c0ffe68a76d06bde0e8e01fba7ad45bfcb
Pablo Riesgo Ferreiro <priesgoferreiro@gmail.com>
2020-08-03 12:22:28 +0200

    89 create new user individuals (#116)
    * add endpoint to post user_individuals
    
    * blackened

---
+++ views/helpers.py
@@ -0,0 +1,24 @@
+from flask import request
+
+from views import application
+from views.exceptions import PhenopolisException
+
+
+def _get_json_payload():
+    if not request.is_json:
+        raise PhenopolisException("Only mimetype application/json is accepted")
+    payload = request.get_json(silent=True)
+    if payload is None:
+        raise PhenopolisException("Empty payload or wrong formatting")
+    application.logger.debug(payload)
+    return payload
+
+
+def _parse_payload(payload, model_class):
+    if isinstance(payload, dict):
+        objects = [model_class(**payload)]
+    elif isinstance(payload, list):
+        objects = [model_class(**p) for p in payload]
+    else:
+        raise PhenopolisException("Payload of unexpected type: {}".format(type(payload)))
+    return objects
