d266315d2c25fdd9dfa3e2b2f3e7bf5668ebdf62
tamarkhachatryan <khachatryantamar@gmail.com>
2020-08-19 17:47:00 +0400

    Moved styles to SCSS and used functional component instead of class based component. (#201)

--- src/page/Login.js
+++ src/page/Login.js
@@ -1,96 +1,62 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import compose from 'recompose/compose';
-
-import { connect } from 'react-redux';
-import { setUser } from '../redux/actions/users';
-import { withStyles } from '@material-ui/core/styles';
+import { useSelector } from 'react-redux';
 import { CssBaseline, Typography, Container, Paper, Box } from '@material-ui/core';
-
 import LoginBox from '../components/AppBar/LoginBox';
+import { Trans, useTranslation } from 'react-i18next';
 
-import { withTranslation, Trans } from 'react-i18next';
-
-class Login extends React.Component {
-  getReduxName() {
-    return this.props.reduxName;
-  }
-
-  render() {
-    const { classes } = this.props;
-    const { t } = this.props;
-    const query = new URLSearchParams(this.props.location.search);
-    const mylink = query.get('link');
-
-    return (
-      <>
-        <CssBaseline />
-        {this.props.reduxName === '' ? (
-          <div className={classes.root}>
-            {query.get('link') ? (
-              mylink === 'timeout' ? (
+const Login = (props) => {
+  const { reduxName } = useSelector((state) => ({
+    reduxName: state.users.username
+  }));
+  const { t } = useTranslation();
+  const query = new URLSearchParams(props.location.search);
+  const mylink = query.get('link');
+  return (
+    <>
+      <CssBaseline />
+      {reduxName === '' ? (
+        <div className={'login-root'}>
+          {query.get('link') ? (
+            mylink === 'timeout' ? (
+              <span>
+                {' '}
+                <b style={{ color: '#2E84CF' }}> Token Expired </b>, please re-login.
+              </span>
+            ) : (
+              <Trans i18nKey="Login.redirectionLink">
                 <span>
                   {' '}
-                  <b style={{ color: '#2E84CF' }}> Token Expired </b>, please re-login.
+                  Oops, seems you don't have access to{' '}
+                  <b style={{ color: '#2E84CF' }}> {{ mylink }} </b> yet, try login?
                 </span>
-              ) : (
-                <Trans i18nKey="Login.redirectionLink">
-                  <span>
-                    {' '}
-                    Oops, seems you don't have access to{' '}
-                    <b style={{ color: '#2E84CF' }}> {{ mylink }} </b> yet, try login?
-                  </span>
-                </Trans>
-              )
-            ) : null}
+              </Trans>
+            )
+          ) : null}
 
-            <LoginBox
-              onLoginSuccess={() => {}}
-              redirectLink={
-                query.get('link') & (query.get('link') !== 'timeout') ? query.get('link') : '/'
-              }></LoginBox>
-          </div>
-        ) : (
-          <div className={classes.root}>
-            <Container maxWidth="md">
-              <Paper className={classes.paper2}>
-                <Typography component="div">
-                  <Box fontWeight="900" fontSize="h4.fontSize" m={1}>
-                    {t('Login.login')}
-                  </Box>
-                  <Box fontWeight="fontWeightLight" m={1}>
-                    {t('Login.login_subtitle')}
-                  </Box>
-                </Typography>
-              </Paper>
-            </Container>
-          </div>
-        )}
-      </>
-    );
-  }
+          <LoginBox
+            onLoginSuccess={() => {}}
+            redirectLink={
+              query.get('link') & (query.get('link') !== 'timeout') ? query.get('link') : '/'
+            } />
+        </div>
+      ) : (
+        <div className={'login-root'}>
+          <Container maxWidth="md">
+            <Paper className={'login-paper2'}>
+              <Typography component="div">
+                <Box fontWeight="900" fontSize="h4.fontSize" m={1}>
+                  {t('Login.login')}
+                </Box>
+                <Box fontWeight="fontWeightLight" m={1}>
+                  {t('Login.login_subtitle')}
+                </Box>
+              </Typography>
+            </Paper>
+          </Container>
+        </div>
+      )}
+    </>
+  );
 }
 
-Login.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-const styles = (theme) => ({
-  root: {
-    height: 'calc(100vh - 64px)',
-    position: 'relative',
-    backgroundColor: '#eeeeee',
-    padding: '4em',
-    textAlign: 'center',
-  },
-  paper2: {
-    padding: theme.spacing(5),
-  },
-});
-
-const mapStateToProps = (state) => ({ reduxName: state.users.username });
-export default compose(
-  withStyles(styles),
-  connect(mapStateToProps, { setUser }),
-  withTranslation()
-)(Login);
+export default Login;
