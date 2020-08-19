f29a6db9561a902fc8766b71711b861215f91dce
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-08-19 17:17:55 +0100

    Added scripts wrapper to patch dev/prod databases

---
+++ scripts/patch_prod_db.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Apply outstanding database migrations to the phenopolis prod_db database
+#
+# Usage:
+#
+# Just run this script on the phenopolis_api host. Extra params are added to
+# patch_db.py (e.g. --yes for unattended migration)
+
+set -euo pipefail
+
+export PGDATABASE=phenopolis_prod_db
+
+dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+exec "${dir}/patch_dev_db.sh" "$@"
