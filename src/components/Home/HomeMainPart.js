2d4e4954c567fd56b57f6a1522f4ae38070d3151
tamarkhachatryan <khachatryantamar@gmail.com>
2020-08-24 16:47:04 +0400

    Moved styles from component to scss. (#224)

--- src/components/Home/HomeMainPart.js
+++ src/components/Home/HomeMainPart.js
@@ -9,57 +9,13 @@ import {
 } from '@fortawesome/pro-duotone-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { makeStyles } from '@material-ui/styles';
 import { useTranslation, Trans } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { getStatistics } from '../../redux/actions/statistic';
 import { setSnack } from '../../redux/actions/snacks';
 
-const useStyles = makeStyles({
-  root: {
-    flexGrow: 1,
-    backgroundColor: 'white',
-    padding: '4em 0em 4em 0em',
-  },
-  bannertext: {
-    textAlign: 'center',
-    color: 'white',
-  },
-  paper: {
-    padding: '4em 1em 4em 1em',
-  },
-  gridpaper: {
-    textAlign: 'center',
-  },
-  fontawesomeicon: {
-    fontSize: 50,
-    marginBottom: '0.3em',
-  },
-  root2: {
-    flexGrow: 1,
-    backgroundColor: '#eeeeee',
-    padding: '4em 0em 4em 0em',
-  },
-  gridpaper2: {
-    textAlign: 'center',
-  },
-  paper2: {
-    padding: '4em 1em 4em 1em',
-    backgroundColor: '#eeeeee',
-  },
-  a: {
-    textDecoration: 'none',
-    color: '#2E84CF',
-    fontWeight: '900',
-    '&:hover': {
-      textShadow: '-0.06ex 0 white, 0.06ex 0 white',
-    },
-  },
-});
-
 const HomeMainPart = () => {
   const { t } = useTranslation();
-  const classes = useStyles();
   const dispatch = useDispatch();
   const { error, loading, exomes, variants } = useSelector((state) => ({
     variants: state.Statistics.data.total_variants,
@@ -77,14 +33,14 @@ const HomeMainPart = () => {
 
   return (
     <div>
-      <Grid container className={classes.root}>
+      <Grid container className={'home-root'}>
         <Grid container justify="center">
-          <Grid item xs={12} md={3} className={classes.gridpaper}>
-            <Paper elevation={0} className={classes.paper}>
+          <Grid item xs={12} md={3} className={'home-gridpaper'}>
+            <Paper elevation={0} className={'home-paper'}>
               <FontAwesomeIcon
                 icon={faTachometerAlt}
                 color="#2E84CF"
-                className={classes.fontawesomeicon}
+                className={'home-fontawesomeicon'}
               />
               <Typography component="div">
                 <Box fontWeight="900" fontSize="h5.fontSize" m={1}>
@@ -97,12 +53,12 @@ const HomeMainPart = () => {
             </Paper>
           </Grid>
 
-          <Grid item xs={12} md={3} className={classes.gridpaper}>
-            <Paper elevation={0} className={classes.paper}>
+          <Grid item xs={12} md={3} className={'home-gridpaper'}>
+            <Paper elevation={0} className={'home-paper'}>
               <FontAwesomeIcon
                 icon={faChartBar}
                 color="#2E84CF"
-                className={classes.fontawesomeicon}
+                className={'home-fontawesomeicon'}
               />
               <Typography component="div">
                 <Box fontWeight="900" fontSize="h5.fontSize" m={1}>
@@ -115,12 +71,12 @@ const HomeMainPart = () => {
             </Paper>
           </Grid>
 
-          <Grid item xs={12} md={3} className={classes.gridpaper}>
-            <Paper elevation={0} className={classes.paper}>
+          <Grid item xs={12} md={3} className={'home-gridpaper'}>
+            <Paper elevation={0} className={'home-paper'}>
               <FontAwesomeIcon
                 icon={faProjectDiagram}
                 color="#2E84CF"
-                className={classes.fontawesomeicon}
+                className={'home-fontawesomeicon'}
               />
               <Typography component="div">
                 <Box fontWeight="900" fontSize="h5.fontSize" m={1}>
@@ -135,11 +91,11 @@ const HomeMainPart = () => {
         </Grid>
       </Grid>
 
-      <Grid container className={classes.root2}>
+      <Grid container className={'home-root2'}>
         <Grid container justify="center">
-          <Grid item xs={12} md={8} className={classes.gridpaper2}>
-            <Paper elevation={0} className={classes.paper2}>
-              <FontAwesomeIcon icon={faUsers} color="#2E84CF" className={classes.fontawesomeicon} />
+          <Grid item xs={12} md={8} className={'home-gridpaper2'}>
+            <Paper elevation={0} className={'home-paper2'}>
+              <FontAwesomeIcon icon={faUsers} color="#2E84CF" className={'home-fontawesomeicon'} />
               <Typography component="div">
                 <Box fontWeight="900" fontSize="h4.fontSize" m={1}>
                   {t('HomePage.HomeMainPart.statistic.title')}
@@ -160,14 +116,14 @@ const HomeMainPart = () => {
         </Grid>
       </Grid>
 
-      <Grid container className={classes.root}>
+      <Grid container className={'home-root'}>
         <Grid container justify="center">
-          <Grid item xs={12} md={8} className={classes.gridpaper}>
-            <Paper elevation={0} className={classes.paper}>
+          <Grid item xs={12} md={8} className={'home-gridpaper'}>
+            <Paper elevation={0} className={'home-paper'}>
               <FontAwesomeIcon
                 icon={faEnvelopeOpen}
                 color="#2E84CF"
-                className={classes.fontawesomeicon}
+                className={'home-fontawesomeicon'}
               />
               <Typography component="div">
                 <Box fontWeight="900" fontSize="h4.fontSize" m={1}>
@@ -176,7 +132,7 @@ const HomeMainPart = () => {
                 <Box fontWeight="fontWeightLight" fontSize="h6.fontSize" m={1}>
                   <Trans i18nKey="HomePage.HomeMainPart.contact.description">
                     Please feel free to contact us{' '}
-                    <a href="mailto:info@phenopolis.org" className={classes.a}>
+                    <a href="mailto:info@phenopolis.org" className={'home-link'}>
                       here
                     </a>{' '}
                     to give us feedback or report any issues.
