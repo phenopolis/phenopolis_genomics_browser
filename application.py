66be02c54e9905b47a201bbc46adfda097985f76
Alan Silva <alanwilter@gmail.com>
2020-09-04 12:54:25 +0200

    For using coverage with APP_ENV test

--- application.py
+++ application.py
@@ -1,9 +1,15 @@
 """
 Flask app
 """
-
-from views import application
+from views import application, APP_ENV
 
 if __name__ == "__main__":
-    application.debug = True
+    application.debug = False
+    if APP_ENV == "debug":
+        application.debug = True
+    elif APP_ENV == "test":
+        from pytest_cov.embed import cleanup_on_sigterm
+
+        cleanup_on_sigterm()
+
     application.run()
