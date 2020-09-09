11d046167ab8d1e21d5103109f7e2a29b511a066
priesgof <priesgoferreiro@gmail.com>
2020-09-09 22:53:52 +0200

    trying to fix tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -20,16 +20,16 @@ def test_get_user(_demo):
     assert res[0].data is not None
     user_dict = json.loads(res[0].data)
     assert isinstance(user_dict, dict)
-    assert user_dict.get("user") == "Admin"
-    assert user_dict.get("argon_password") is None
-    individual_ids = user_dict.get("individuals")
-    assert isinstance(individual_ids, list)
-    assert len(individual_ids) > 0
+    assert user_dict.get("user") == "Admin", "user_dict={}".format(user_dict)
+    assert user_dict.get("argon_password") is None, "user_dict={}".format(user_dict)
+    individual_ids = user_dict.get("individuals"), "user_dict={}".format(user_dict)
+    assert isinstance(individual_ids, list), "user_dict={}".format(user_dict)
+    assert len(individual_ids) > 0, "user_dict={}".format(user_dict)
 
 
 def test_get_non_existing_user(_admin):
     """res -> tuple(flask.wrappers.Response)"""
     res = get_user("JuanSinMiedo")
     assert res[0].status_code == 200
-    assert "The user does not exist" in res[0].data
+    assert "The user does not exist" in res
     assert res[1] == 404
