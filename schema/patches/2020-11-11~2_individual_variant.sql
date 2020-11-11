44cac8a52b9b6a0f4bae6d1f8d1538f9760530f4
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-11-11 02:54:26 +0000

    Added individual-variant join table with zigosity
    The migration script handles both internal ids and external ids in the
    hom/het tables, but always stores internal_id.
    
    It's probably a good idea to re-run migrate_variants before running this
    migrate_individual_variants script.

---
+++ schema/patches/2020-11-11~2_individual_variant.sql
@@ -0,0 +1,19 @@
+set search_path to phenopolis, public;
+
+create table individual_variant (
+    individual_id text not null,
+    variant_id bigint not null references variant (id),
+    primary key (variant_id, individual_id),
+
+    chrom text not null,
+    pos int4 not null,
+    ref text not null,
+    alt text not null,
+
+    zygosity text not null check (zygosity in ('HOM', 'HET'))
+);
+
+create index on individual_variant (individual_id);
+create index on individual_variant (pos);
+
+reset search_path;
--- schema/phenopolis.sql
+++ schema/phenopolis.sql
@@ -24,3 +24,20 @@ create index on transcript_consequence (pos, chrom);
 create index on transcript_consequence (hgvs_c);
 create index on transcript_consequence (hgvs_p);
 create index on transcript_consequence (gene_id);
+
+
+create table individual_variant (
+    individual_id text not null,
+    variant_id bigint not null references variant (id),
+    primary key (variant_id, individual_id),
+
+    chrom text not null,
+    pos int4 not null,
+    ref text not null,
+    alt text not null,
+
+    zygosity text not null check (zygosity in ('HOM', 'HET'))
+);
+
+create index on individual_variant (individual_id);
+create index on individual_variant (pos);
