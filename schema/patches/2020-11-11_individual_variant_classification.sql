1d2599a03459aa7530b14ffede5043e4d580f740
priesgof <priesgoferreiro@gmail.com>
2020-11-11 21:07:14 +0100

    apply requested changes + patch

---
+++ schema/patches/2020-11-11_individual_variant_classification.sql
@@ -0,0 +1,20 @@
+set search_path to phenopolis, public;
+
+create table individual_variant_classification (
+    id bigserial primary key,
+    individual_id text not null,
+    variant_id bigint not null references variant(id),
+    user_id not null,
+    classified_on timestamp with time zone,
+    -- this represents the ACMG classification
+    classification text not null check (classification in ('pathogenic', 'likely_pathogenic', 'benign', 'likely_benign', 'unknown_significance'))
+    pubmed_id text,
+    notes text
+);
+
+create index on individual_variant_classification(user_id);
+create index on individual_variant_classification(individual_id, variant_id);
+create index on individual_variant_classification(variant_id);
+create index on individual_variant_classification(pubmed_id);
+
+reset search_path;
--- schema/phenopolis.sql
+++ schema/phenopolis.sql
@@ -46,7 +46,7 @@ create table individual_variant_classification (
     id bigserial primary key,
     individual_id text not null,
     variant_id bigint not null references variant(id),
-    "user" not null references public.users("user"),
+    user_id not null,
     classified_on timestamp with time zone,
     -- this represents the ACMG classification
     classification text not null check (classification in ('pathogenic', 'likely_pathogenic', 'benign', 'likely_benign', 'unknown_significance'))
@@ -54,6 +54,7 @@ create table individual_variant_classification (
     notes text
 );
 
-create index on individual_variant_classification("user");
+create index on individual_variant_classification(user_id);
 create index on individual_variant_classification(individual_id, variant_id);
 create index on individual_variant_classification(variant_id);
+create index on individual_variant_classification(pubmed_id);
