d4bb43a676a49eabc7f11e0c054f582e86d841f4
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-11-17 17:30:34 +0000

    Added extra fields to individual_variant (#232)
    * Added extra fields to individual_variant
    
    See #157
    
    Also added script to validate the syntax of the schema file.
    
    * Trigger name more generic to use on other tables too
    
    * Added individual-gene mapping table
    
    No import script just yet, as I need to see the ensembl data.
    
    See #158

--- schema/phenopolis.sql
+++ schema/phenopolis.sql
@@ -36,8 +36,51 @@ create table individual_variant (
     ref text not null,
     alt text not null,
 
-    zygosity text not null check (zygosity in ('HOM', 'HET'))
+    zygosity text not null check (zygosity in ('HOM', 'HET')),
+    status text,
+    clinvar_id text,
+    pubmed_id text,
+    comment text,
+    user_id text,
+    timestamp timestamptz not null
 );
 
 create index on individual_variant (individual_id);
 create index on individual_variant (pos);
+create index on individual_variant (clinvar_id);
+create index on individual_variant (pubmed_id);
+
+-- Update the timestamp both on insert and update
+create function timestamp_update() returns trigger
+language plpgsql as $$
+begin
+    new.timestamp = now();
+    return new;
+end
+$$;
+
+create trigger timestamp_update
+before insert or update on individual_variant
+for each row execute procedure timestamp_update();
+
+
+create table individual_gene (
+    individual_id text not null,
+    gene_id bigint not null references ensembl.gene (identifier),
+    primary key (gene_id, individual_id),
+
+    status text,
+    clinvar_id text,
+    pubmed_id text,
+    comment text,
+    user_id text,
+    timestamp timestamptz not null
+);
+
+create index on individual_gene (individual_id);
+create index on individual_gene (clinvar_id);
+create index on individual_gene (pubmed_id);
+
+create trigger timestamp_update
+before insert or update on individual_gene
+for each row execute procedure timestamp_update();
