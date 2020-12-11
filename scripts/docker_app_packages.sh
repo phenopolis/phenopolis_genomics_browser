da018fd54a63cd5e2cbda7b2321367d119a483ee
Alan Silva <alanwilter@gmail.com>
2020-12-11 14:04:00 +0100

    gcc and openssl changed

--- scripts/docker_app_packages.sh
+++ scripts/docker_app_packages.sh
@@ -8,10 +8,10 @@ set -x
 yum update -y
 yum install -y --setopt install_weak_deps=false \
     bzip2-devel-1.0.6-13.amzn2.0.2 \
-    gcc-7.3.1-9.amzn2 \
+    gcc-7.3.1-11.amzn2 \
     libcurl-devel-7.61.1-12.amzn2.0.2 \
     make-3.82-24.amzn2 \
-    openssl-devel-1.0.2k-19.amzn2.0.3 \
+    openssl-devel-1.0.2k-19.amzn2.0.4 \
     python3-3.7.9-1.amzn2.0.1 python3-pip-9.0.3-1.amzn2.0.2 \
     python3-devel-3.7.9-1.amzn2.0.1 \
     python3-wheel-0.30.0a0-9.amzn2.0.3 \
@@ -22,7 +22,7 @@ pip3 --no-cache-dir install --upgrade pip
 pip --no-cache-dir install gunicorn==20.0.4
 pip --no-cache-dir install -r requirements.txt
 pip --no-cache-dir install cyvcf2==0.30.1
-pip --no-cache-dir install cyvcf2==0.30.1 --force-reinstall
+#pip --no-cache-dir install cyvcf2==0.30.1 --force-reinstall
 
 yum -y erase \
     bzip2-devel gcc libcurl-devel make openssl-devel perl python3-devel \
