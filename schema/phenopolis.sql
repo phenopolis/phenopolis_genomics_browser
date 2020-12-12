030220fc5eba98ffec24b6f8aa52401ec0cfe773
Daniele Varrazzo <daniele.varrazzo@gmail.com>
2020-12-12 16:07:57 +0100

    Added sketch of individual table

--- schema/phenopolis.sql
+++ schema/phenopolis.sql
@@ -26,8 +26,43 @@ create index on transcript_consequence (hgvs_p);
 create index on transcript_consequence (gene_id);
 
 
+create table individual (
+    id serial primary key,
+    phenopolis_id text not null unique check (phenopolis_id ~ '^PH\d+$'),
+    constraint ids_match check (id = replace(phenopolis_id, 'PH', '')::int),
+    external_id text unique,
+    sex text check (sex in ('M', 'F', 'N'))
+);
+
+create or replace function individual_phenopolis_id_update() returns trigger
+language plpgsql as $$
+begin
+    new.phenopolis_id = 'PH' || to_char(new.id, 'FM00000000');
+    return new;
+end
+$$;
+
+create trigger individual_phenopolis_id_update
+before insert or update on individual
+for each row execute procedure individual_phenopolis_id_update();
+
+create index on individual (phenopolis_id);
+create index on individual (external_id);
+
+
+create table individual_feature (
+    individual_id int references individual (id),
+    feature_id int not null references hpo.term (id),
+    type text not null
+        check (type in ('observed', 'unobserved', 'simplified', 'ancestor')),
+    primary key (individual_id, feature_id, type)
+);
+
+create index on individual_feature (feature_id);
+
+
 create table individual_variant (
-    individual_id text not null,
+    individual_id text not null,    -- TODO: fkey to individual after data migration
     variant_id bigint not null references variant (id),
     primary key (variant_id, individual_id),
 
---
+++ scripts/migrate_individuals.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Migrate variants data from `public.individuals` to `phenopolis.individual`
+# and `phenopolis.individual_feature`.
+#
+# Usage:
+#
+# Pass the script the psql connection params.
+#
+#    migrate_individuals.sh "host=HOST user=USER dbname=DBNAME"
+
+set -euo pipefail
+# set -x
+
+psql -e1X "$@" <<HERE
+set work_mem = '1GB';
+
+insert into phenopolis.individual (id, sex)
+select replace(internal_id, 'PH', '')::int, nullif(sex, 'U')
+from public.individuals
+on conflict on constraint individual_phenopolis_id_key do nothing;
+
+select setval('phenopolis.individual_id_seq',
+    (select max(id) from phenopolis.individual));
+
+
+update phenopolis.individual
+set external_id = old.external_id
+from individuals old
+where old.internal_id = phenopolis_id;
+
+
+insert into phenopolis.individual_feature (individual_id, feature_id, type)
+select i.id, f.id, 'observed'
+from phenopolis.individual i
+join (
+    select internal_id, unnest(string_to_array(observed_features, ',')) as hpo_id
+    from public.individuals
+) as j on j.internal_id = i.phenopolis_id
+join hpo.term f on f.hpo_id = j.hpo_id
+on conflict on constraint individual_feature_pkey do nothing;
+
+insert into phenopolis.individual_feature (individual_id, feature_id, type)
+select i.id, f.id, 'unobserved'
+from phenopolis.individual i
+join (
+    select internal_id, unnest(string_to_array(unobserved_features, ',')) as hpo_id
+    from public.individuals
+) as j on j.internal_id = i.phenopolis_id
+join hpo.term f on f.hpo_id = j.hpo_id
+on conflict on constraint individual_feature_pkey do nothing;
+
+insert into phenopolis.individual_feature (individual_id, feature_id, type)
+select i.id, f.id, 'simplified'
+from phenopolis.individual i
+join (
+    select internal_id, unnest(string_to_array(simplified_observed_features, ',')) as hpo_id
+    from public.individuals
+) as j on j.internal_id = i.phenopolis_id
+join hpo.term f on f.hpo_id = j.hpo_id
+on conflict on constraint individual_feature_pkey do nothing;
+
+insert into phenopolis.individual_feature (individual_id, feature_id, type)
+select i.id, f.id, 'ancestor'
+from phenopolis.individual i
+join (
+    select internal_id, unnest(string_to_array(ancestor_observed_features, ',')) as hpo_id
+    from public.individuals
+) as j on j.internal_id = i.phenopolis_id
+join hpo.term f on f.hpo_id = j.hpo_id
+on conflict on constraint individual_feature_pkey do nothing;
+
+
+analyze phenopolis.individual, phenopolis.individual_feature;
+HERE
