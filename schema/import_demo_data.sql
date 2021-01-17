287ae52cc466ea6c21b6df97b71b663c3d5fe2ae
Alan Silva <alanwilter@gmail.com>
2021-01-17 18:00:24 +0100

    New tables for new schema to load in Demo DB

--- schema/import_demo_data.sql
+++ schema/import_demo_data.sql
@@ -1,4 +1,10 @@
-
-\copy phenopolis.variant FROM '/app/schema/seed/phenopolis.variant.csv' delimiter ',' CSV HEADER;
+-- order of import matters
+\copy phenopolis.variant (id,chrom,pos,ref,alt) FROM '/app/schema/seed/phenopolis.variant.csv' delimiter ',' CSV HEADER;
 \copy phenopolis.transcript_consequence FROM '/app/schema/seed/phenopolis.transcript_consequence.csv' delimiter ',' CSV HEADER;
 \copy phenopolis.individual_variant (individual_id,variant_id,chrom,pos,ref,alt,zygosity) FROM '/app/schema/seed/phenopolis.individual_variant.csv' delimiter ',' CSV HEADER;
+\copy ensembl.gene FROM '/app/schema/seed/ensembl.gene.csv' delimiter ',' CSV HEADER;
+\copy hpo.term FROM '/app/schema/seed/hpo.term.csv' delimiter ',' CSV HEADER;
+\copy hpo.is_a FROM '/app/schema/seed/hpo.is_a.csv' delimiter ',' CSV HEADER;
+\copy phenopolis.individual FROM '/app/schema/seed/phenopolis.individual.csv' delimiter ',' CSV HEADER;
+\copy phenopolis.individual_feature FROM '/app/schema/seed/phenopolis.individual_feature.csv' delimiter ',' CSV HEADER;
+\copy phenopolis.individual_gene FROM '/app/schema/seed/phenopolis.individual_gene.csv' delimiter ',' CSV HEADER;
