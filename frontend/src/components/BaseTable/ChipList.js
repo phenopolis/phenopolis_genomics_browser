93e35eacd48d5179e3743166493f1123c53e8575
YuanTian1991 <tianyuan1991hit@gmail.com>
2021-01-22 00:03:29 +0000

    BaseTable works for Chip, but it's a bit slow than old table, we need to decide if we want to continue use it.

---
+++ frontend/src/components/BaseTable/ChipList.js
@@ -0,0 +1,60 @@
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import TypeChip from '../Chip/TypeChip';
+import { Chip } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+
+}));
+
+const ChipList = (props) => {
+  const classes = useStyles();
+
+  useEffect(() => {
+    // console.log(props)
+  }, [props]);
+
+  return (
+    <span >
+      {
+        typeof props.chips === "object" ? (
+          props.chips.map((chip, index) => {
+            return (
+              <TypeChip
+                key={index}
+                label={chip.display}
+                type={props.colName.base_href.replace(/[^a-zA-Z0-9_-]/g, '')}
+                size="small"
+                action="forward"
+                popover={true}
+                to={
+                  chip.end_href
+                    ? (props.colName.base_href + '/' + chip.end_href).replace(/\/\//g, '/')
+                    : (props.colName.base_href + '/' + chip.display).replace(/\/\//g, '/')
+                }
+              />
+
+              // <Chip
+              //   key={index}
+              //   label={chip.display}
+              //   // onClick={handleClick}
+              //   // onDelete={handleDelete}
+              //   // deleteIcon={<DoneIcon />}
+              //   variant="outlined"
+              //   style={{ margin: '2px' }}
+              // />
+            )
+          })
+        ) : (
+            // This is not good, some columns like Rsid was labelled "links", but actually it's just a string (not object) in this attribute.
+            <>
+              {props.chips}
+            </>
+          )
+      }
+    </span>
+  )
+}
+
+export default ChipList
--- frontend/src/components/BaseTable/VersatileTable.js
+++ frontend/src/components/BaseTable/VersatileTable.js
@@ -3,194 +3,70 @@ import React, { useEffect, useState } from 'react';
 import BaseTable, { Column, AutoResizer } from 'react-base-table';
 import 'react-base-table/styles.css';
 
-var faker = require('faker');
-
-const dataGenerator = () => ({
-  id: faker.random.uuid(),
-  name: faker.name.findName(),
-  gender: faker.random.boolean() ? 'male' : 'female',
-  score: {
-    math: faker.random.number(70) + 30,
-  },
-  birthday: faker.date.between(1995, 2005),
-  attachments: faker.random.number(5),
-  description: faker.lorem.sentence(),
-  email: faker.internet.email(),
-  country: faker.address.country(),
-  address: {
-    street: faker.address.streetAddress(),
-    city: faker.address.city(),
-    zipCode: faker.address.zipCode(),
-  },
-});
-
-// const GenderContainer = styled.div`
-//   background-color: ${props =>
-//     props.gender === 'male' ? 'lightblue' : 'pink'};
-//   color: white;
-//   border-radius: 3px;
-//   width: 20px;
-//   height: 20px;
-//   font-size: 16px;
-//   font-weight: bold;
-//   line-height: 20px;
-//   text-align: center;
-// `
-
-const Gender = ({ gender }) => <div gender={gender}>{gender === 'male' ? '♂' : '♀'}</div>;
-
-// const Score = styled.span`
-//   color: ${props => (props.score >= 60 ? 'green' : 'red')};
-// `
-
-// const Attachment = styled.div`
-//   background-color: lightgray;
-//   width: 20px;
-//   height: 20px;
-//   line-height: 20px;
-//   text-align: center;
-//   border-radius: 4px;
-//   color: gray;
-// `
-
-const defaultData = new Array(5000)
-  .fill(0)
-  .map(dataGenerator)
-  .sort((a, b) => (a.name > b.name ? 1 : -1));
-
-const defaultSort = { key: 'name', order: 'asc' };
-
-const columns = [
-  {
-    key: 'name',
-    title: 'Name',
-    dataKey: 'name',
-    width: 150,
-    resizable: true,
-    sortable: true,
-    frozen: Column.FrozenDirection.LEFT,
-  },
-  {
-    key: 'score',
-    title: 'Score',
-    dataKey: 'score.math',
-    width: 60,
-    align: Column.Alignment.CENTER,
-    sortable: false,
-  },
-  {
-    key: 'gender',
-    title: '♂♀',
-    dataKey: 'gender',
-    cellRenderer: ({ cellData: gender }) => <Gender gender={gender} />,
-    width: 60,
-    align: Column.Alignment.CENTER,
-    sortable: true,
-  },
-  {
-    key: 'birthday',
-    title: 'Birthday',
-    dataKey: 'birthday',
-    dataGetter: ({ column, rowData }) => rowData[column.dataKey].toLocaleDateString(),
-    width: 100,
-    align: Column.Alignment.RIGHT,
-    sortable: true,
-  },
-  {
-    key: 'attachments',
-    title: 'Attachments',
-    dataKey: 'attachments',
-    width: 60,
-    align: Column.Alignment.CENTER,
-    headerRenderer: () => <div>?</div>,
-    cellRenderer: ({ cellData }) => <div>{cellData}</div>,
-  },
-  {
-    key: 'description',
-    title: 'Description',
-    dataKey: 'description',
-    width: 200,
-    resizable: true,
-    sortable: true,
-    cellRenderer: ({ cellData }) => <div>{cellData}</div>,
-  },
-  {
-    key: 'email',
-    title: 'Email',
-    dataKey: 'email',
-    width: 200,
-    resizable: true,
-    sortable: true,
-  },
-  {
-    key: 'country',
-    title: 'Country',
-    dataKey: 'country',
-    width: 100,
-    resizable: true,
-    sortable: true,
-  },
-  {
-    key: 'address',
-    title: 'Address',
-    dataKey: 'address.street',
-    width: 200,
-    resizable: true,
-  },
-  // {
-  //   key: 'action',
-  //   width: 100,
-  //   align: Column.Alignment.CENTER,
-  //   frozen: Column.FrozenDirection.RIGHT,
-  //   cellRenderer: ({ rowData }) => (
-  //     <button
-  //       onClick={() => {
-  //         this.setState({
-  //           data: this.state.data.filter(x => x.id !== rowData.id),
-  //         })
-  //       }}
-  //     >
-  //       Remove
-  //     </button>
-  //   ),
-  // },
-];
+import ChipList from './ChipList'
 
 const VersatileTable = (props) => {
-  const [tableData, setTableData] = useState(defaultData);
-  const [sortBy, setSortBy] = useState(defaultSort);
-  const [tableColumn, setTableColumn] = useState(columns);
+  const [tableData, setTableData] = useState([]);
+  const [sortBy, setSortBy] = useState(null);
+  const [tableColumn, setTableColumn] = useState([]);
+  const [ready, setReady] = useState(false);
 
   useEffect(() => {
-    console.log(tableData);
+    // console.log(props.tableData);
     if (props.tableData) {
-      console.log(props.tableData);
+      // console.log(props.tableData);
       modifyData(props.tableData);
     }
   }, [props.tableData]);
 
   const modifyData = (tableData) => {
-    console.log(tableData);
+    // console.log(tableData);
     const tmpColumn = tableData.colNames.map((col) => {
-      return {
-        key: col.key,
-        title: col.name,
-        dataKey: col.key,
-        width: 150,
-        resizable: true,
-        align: Column.Alignment.CENTER,
-        sortable: true,
-      };
+      if (col.type == "links") {
+        return {
+          key: col.key,
+          title: col.name,
+          dataKey: col.key,
+          base_href: col.base_href,
+          width: 600,
+          resizable: true,
+          align: Column.Alignment.CENTER,
+          sortable: true,
+          cellRenderer: ({ cellData, column }) => <ChipList chips={cellData} colName={column} />,
+        };
+      } else {
+        return {
+          key: col.key,
+          title: col.name,
+          dataKey: col.key,
+          width: 200,
+          resizable: true,
+          align: Column.Alignment.CENTER,
+          sortable: true,
+        };
+      }
     });
 
+    const tmpData = tableData.data.map((row, rowIndex) => {
+      return (
+        {
+          ...row,
+          id: rowIndex
+        }
+      )
+    })
+
+    console.log(tmpData)
+
     setTableColumn(tmpColumn);
-    setTableData(tableData.data);
+    setTableData(tmpData);
     setSortBy({ key: 'CHROM', order: 'asc' });
+    setReady(true)
   };
 
   const onColumnSort = (sortBy) => {
-    console.log(sortBy);
-    console.log(tableData);
+    // console.log(sortBy);
+    // console.log(tableData);
     const order = sortBy.order === 'asc' ? 1 : -1;
     const tmpdata = [...tableData];
     tmpdata.sort((a, b) => (a[sortBy.key] > b[sortBy.key] ? order : -order));
@@ -198,21 +74,41 @@ const VersatileTable = (props) => {
     setSortBy(sortBy);
   };
 
+  const estRowHight = (rowData, rowIndex) => {
+    if (rowIndex % 3 === 1) {
+      return 50
+    } else {
+      return 200
+    }
+  }
+
   return (
     <div style={{ width: '100%', height: '50vh', marginTop: '2em' }}>
-      <AutoResizer>
-        {({ width, height }) => (
-          <BaseTable
-            width={width}
-            height={height}
-            fixed
-            columns={tableColumn}
-            data={tableData}
-            sortBy={sortBy}
-            onColumnSort={onColumnSort}
-          />
-        )}
-      </AutoResizer>
+      {
+        ready ? (
+          <AutoResizer>
+            {({ width, height }) => (
+              <BaseTable
+                width={width}
+                height={height}
+                fixed
+                rowKey="id"
+                // estimatedRowHeight={({ rowData, rowIndex }) => estRowHight(rowData, rowIndex)}
+                estimatedRowHeight={61}
+                columns={tableColumn}
+                data={tableData}
+                sortBy={sortBy}
+                onColumnSort={onColumnSort}
+              />
+            )}
+          </AutoResizer>
+        ) : (
+            <div>
+              <h2> Loading Data... </h2>
+            </div>
+          )
+      }
+
     </div>
   );
 };
