edfade2a76d90d415a42fe2da660001ee21a8c9c
Alan Silva <alanwilter@gmail.com>
2021-01-28 13:13:37 +0100

    Added test_presign_S3

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -25,3 +25,16 @@ def test_login_logout(_not_logged_in_client):
 
     resp = _not_logged_in_client.get("/is_logged_in")
     assert resp.status_code == 401
+
+
+def test_presign_S3(_admin_client):
+    patientID = "PH0001"
+    payload = {
+        "prefix": patientID,
+        "filename": patientID + "_" + "a_file.vcf",
+        "contentType": "multipart/form-data",
+    }
+    resp = _admin_client.post("/preSignS3URL", json=payload, content_type="application/json")
+    assert resp.status_code == 200
+    assert "x-amz-credential" in str(resp.json)
+    assert resp.json.get("fields").get("key") == f"{patientID}/{payload['filename']}"
