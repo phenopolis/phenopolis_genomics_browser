3339498ec33be9486cb359daf2f7ddcca3add4a4
Alan Silva <alanwilter@gmail.com>
2021-04-02 22:41:21 +0100

    special hpo query refactored for speed

--- views/hpo.py
+++ views/hpo.py
@@ -6,7 +6,6 @@ from views import application
 from views.auth import requires_auth, USER
 from views.postgres import get_db, session_scope
 from views.general import process_for_display, cache_on_browser
-from views.individual import _get_genes_for_individual, _get_feature_for_individual
 from db.helpers import cursor2dict, query_user_config
 
 
@@ -56,16 +55,21 @@ def hpo(hpo_id="HP:0000001", subset="all", language="en"):
         ]
         # query to give the ancestors for a given hpo for a given user for all patients this user has access
         sqlq = """
-            select distinct i.id, i.external_id, i.phenopolis_id, i.sex, i.consanguinity
+            select distinct i.id, i.external_id, i.phenopolis_id, i.sex, i.consanguinity,
+            string_agg(DISTINCT g.hgnc_symbol, ',' ORDER BY g.hgnc_symbol) AS genes,
+            string_agg(DISTINCT concat(t.hpo_id,'@', t."name"), ',' ) AS simplified_observed_features_names
             from hpo.term t join phenopolis.individual_feature if2 on t.id = if2.feature_id
             join phenopolis.individual i on i.id = if2.individual_id
             join public.users_individuals ui on ui.internal_id = i.phenopolis_id
+            left outer join phenopolis.individual_gene ig on ig.individual_id = i.id
+            left outer join ensembl.gene g on g.identifier = ig.gene_id
             where exists (
                 select 1 from hpo.is_a_path p
                 where p.term_id = t.id
                 and p.path ~ %s
             )
             and ui."user" = %s
+            group by i.id, i.external_id, i.phenopolis_id, i.sex, i.consanguinity
             """
         with get_db() as conn:
             with conn.cursor() as cur:
@@ -74,7 +78,7 @@ def hpo(hpo_id="HP:0000001", subset="all", language="en"):
                 individuals = cursor2dict(cur)
 
                 if hpo_id != "HP:0000001":
-                    cur.execute("select * from phenogenon where hpo_id='%s'" % hpo_id)
+                    cur.execute("select * from phenogenon where hpo_id=%s", [hpo_id])
                     config[0]["phenogenon_recessive"]["data"] = [
                         {
                             "gene_id": [{"display": gene_id, "end_href": gene_id}],
@@ -84,18 +88,11 @@ def hpo(hpo_id="HP:0000001", subset="all", language="en"):
                         }
                         for gene_id, hpo_id, hgf, moi_score, in cur.fetchall()
                     ]
-                    cur.execute("select * from phenogenon where hpo_id='%s'" % hpo_id)
-                    config[0]["phenogenon_dominant"]["data"] = [
-                        {
-                            "gene_id": [{"display": gene_id, "end_href": gene_id}],
-                            "hpo_id": hpo_id,
-                            "hgf_score": hgf,
-                            "moi_score": moi_score,
-                        }
-                        for gene_id, hpo_id, hgf, moi_score, in cur.fetchall()
-                    ]
+                    # NOTE: redundant line below commented
+                    # cur.execute("select * from phenogenon where hpo_id=%s", [hpo_id])
+                    config[0]["phenogenon_dominant"]["data"] = config[0]["phenogenon_recessive"]["data"]
                     # Chr,Start,End,HPO,Symbol,ENSEMBL,FisherPvalue,SKATO,variants,CompoundHetPvalue,HWEp,min_depth,nb_alleles_cases,case_maf,nb_ctrl_homs,nb_case_homs,MaxMissRate,nb_alleles_ctrls,nb_snps,nb_cases,minCadd,MeanCallRateCtrls,MeanCallRateCases,OddsRatio,MinSNPs,nb_ctrl_hets,total_maf,MaxCtrlMAF,ctrl_maf,nb_ctrls,nb_case_hets,maxExac
-                    cur.execute("select Symbol,FisherPvalue,SKATO,OddsRatio,variants from skat where HPO='%s'" % hpo_id)
+                    cur.execute("select Symbol,FisherPvalue,SKATO,OddsRatio,variants from skat where HPO= %s", [hpo_id])
                     config[0]["skat"]["data"] = [
                         {
                             "gene_id": [{"display": gene_id, "end_href": gene_id}],
@@ -113,9 +110,12 @@ def hpo(hpo_id="HP:0000001", subset="all", language="en"):
         for ind in individuals:
             ind["internal_id"] = [{"display": ind["phenopolis_id"]}]
             ind["simplified_observed_features_names"] = [
-                {"display": j, "end_href": i} for i, j, in _get_feature_for_individual(ind)
+                {"display": j, "end_href": i}
+                for i, j, in [x.split("@") for x in ind["simplified_observed_features_names"].split(",")]
             ]
-            ind["genes"] = [{"display": i[0]} for i in _get_genes_for_individual(ind)]
+            # ind["genes"] = [{"display": i[0]} for i in _get_genes_for_individual(ind)]
+            if ind["genes"]:
+                ind["genes"] = [{"display": i} for i in ind.get("genes", "").split(",")]
         config[0]["individuals"]["data"] = individuals
         config[0]["metadata"]["data"] = [
             {"name": hpo_name, "id": hpo_id, "count": len(individuals), "parent_phenotypes": parent_phenotypes}
