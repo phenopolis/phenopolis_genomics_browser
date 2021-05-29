6b09476999fdd313e35e4ccbeaac69eaa8bb25af
Yuan Tian <tianyuan1991hit@gmail.com>
2021-05-29 09:24:28 -0700

    Added Skeleton to replace all loading. Except Auth and Translation loading, they will only be loaded once when first time called, there is no way to replace them with skeleton I think. (#358)

--- frontend/src/page/HPO.js
+++ frontend/src/page/HPO.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { CssBaseline, AppBar, Tabs, Tab, Container, Box, Typography } from '@material-ui/core';
-import Loading from '../components/General/Loading';
+import Skeleton from '@material-ui/lab/Skeleton';
 import { useTranslation, Trans } from 'react-i18next';
 import SwipeableViews from 'react-swipeable-views';
 import TabPanel from '../components/Tab/Tabpanel';
@@ -56,136 +56,146 @@ const HPO = (props) => {
   };
 
   return (
-    <>
-      {loaded ? (
-        <React.Fragment>
-          <CssBaseline />
-          <div className="hpo-container">
-            <MetaData
-              metadata={hpoInfo.metadata}
-              name={hpoInfo.metadata.data[0].name + ' - ' + hpoInfo.metadata.data[0].id}
-            />
+    <React.Fragment>
+      <CssBaseline />
+      <div className="hpo-container">
+        {loaded ? (
+          <MetaData
+            metadata={hpoInfo.metadata}
+            name={hpoInfo.metadata.data[0].name + ' - ' + hpoInfo.metadata.data[0].id}
+          />
+        ) : (
+          <Skeleton height={145} />
+        )}
 
-            <Container maxWidth="xl">
-              <AppBar
-                className="hpo-tab_appbar"
-                position="static"
-                color="transparent"
-                elevation={0}
-                m={0}
-                p={0}>
+        {loaded ? (
+          <Container maxWidth="xl">
+            <AppBar
+              className="hpo-tab_appbar"
+              position="static"
+              color="transparent"
+              elevation={0}
+              m={0}
+              p={0}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant="fullWidth"
+                aria-label="full width tabs example"
+                classes={{ indicator: 'hpo-bigIndicator' }}>
+                {[
+                  t('HPO.INDIVIDUALS'),
+                  t('HPO.LITERATURE_GENES'),
+                  t('HPO.PHENOGENON'),
+                  t('HPO.SKAT'),
+                ].map((item, index) => {
+                  return <Tab key={index} label={item} {...a11yProps(index)} />;
+                })}
+              </Tabs>
+            </AppBar>
+          </Container>
+        ) : (
+          <Skeleton height={200} />
+        )}
+
+        {loaded ? (
+          <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
+            <TabPanel value={value} index={0} className="hpo-tabPannel">
+              <VersatileTable
+                tableData={hpoInfo.individuals}
+                title={t('HPO.Individuals')}
+                subtitle={t('HPO.Individuals_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={1} className="hpo-tabPannel">
+              <VersatileTable
+                tableData={hpoInfo.literature_genes}
+                title={t('HPO.Literature_Genes')}
+                subtitle={t('HPO.Literature_Genes_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+
+            {/* Phenogenon tab is more complex. */}
+            <TabPanel value={value} index={2} className="hpo-tabPannel">
+              <Typography component="div">
+                <Box fontWeight="900" fontSize="h4.fontSize" mb={0}>
+                  {t('HPO.Phenogenon')}
+                </Box>
+                <Box fontWeight="fontWeightLight" mb={2}>
+                  {t('HPO.Phenogenon_subtitle')}
+                </Box>
+              </Typography>
+
+              <AppBar position="static" color="transparent" elevation={0} m={0} p={0}>
                 <Tabs
-                  value={value}
-                  onChange={handleChange}
+                  value={phenogenonvalue}
+                  onChange={handleChangePhenogenon}
                   indicatorColor="primary"
                   textColor="primary"
                   variant="fullWidth"
                   aria-label="full width tabs example"
                   classes={{ indicator: 'hpo-bigIndicator' }}>
-                  {[
-                    t('HPO.INDIVIDUALS'),
-                    t('HPO.LITERATURE_GENES'),
-                    t('HPO.PHENOGENON'),
-                    t('HPO.SKAT'),
-                  ].map((item, index) => {
+                  {[t('HPO.RECESSIVE'), t('HPO.DOMINANT')].map((item, index) => {
                     return <Tab key={index} label={item} {...a11yProps(index)} />;
                   })}
                 </Tabs>
               </AppBar>
-            </Container>
+              <SwipeableViews index={phenogenonvalue} onChangeIndex={handleChangePhenogenonIndex}>
+                <TabPanel className="hpo-tabPannel" value={phenogenonvalue} index={0}>
+                  <VersatileTable
+                    tableData={hpoInfo.phenogenon_recessive}
+                    title={t('Recessive')}
+                    subtitle={[
+                      <Trans i18nKey="HPO.RECESSIVE_subtitle">
+                        <b>Genotype</b> : With at least two variants on a given gene that have ExAC
+                        homozygous count not higher than <b style={{ color: '#2E84CF' }}>2</b>, and
+                        CADD phred score not lower than <b style={{ color: '#2E84CF' }}>15</b>.
+                      </Trans>,
+                    ]}
+                    genomePlot={false}
+                  />
+                </TabPanel>
+                <TabPanel className="hpo-tabPannel" value={phenogenonvalue} index={1}>
+                  <VersatileTable
+                    tableData={hpoInfo.phenogenon_dominant}
+                    title={t('Dominant')}
+                    subtitle={[
+                      <Trans i18nKey="HPO.DOMINANT_subtitle">
+                        <b>Genotype</b> : With at least one variant on a given gene that has an ExAC
+                        heterozygous count not higher than ",{' '}
+                        <b style={{ color: '#2E84CF' }}>0.0001</b>, ", and CADD phred score not
+                        lower than ", <b style={{ color: '#2E84CF' }}>15</b>, "."
+                      </Trans>,
+                    ]}
+                    genomePlot={false}
+                  />
+                </TabPanel>
+              </SwipeableViews>
+            </TabPanel>
 
-            <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
-              <TabPanel value={value} index={0} className="hpo-tabPannel">
-                <VersatileTable
-                  tableData={hpoInfo.individuals}
-                  title={t('HPO.Individuals')}
-                  subtitle={t('HPO.Individuals_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-              <TabPanel value={value} index={1} className="hpo-tabPannel">
-                <VersatileTable
-                  tableData={hpoInfo.literature_genes}
-                  title={t('HPO.Literature_Genes')}
-                  subtitle={t('HPO.Literature_Genes_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-
-              {/* Phenogenon tab is more complex. */}
-              <TabPanel value={value} index={2} className="hpo-tabPannel">
-                <Typography component="div">
-                  <Box fontWeight="900" fontSize="h4.fontSize" mb={0}>
-                    {t('HPO.Phenogenon')}
-                  </Box>
-                  <Box fontWeight="fontWeightLight" mb={2}>
-                    {t('HPO.Phenogenon_subtitle')}
-                  </Box>
-                </Typography>
-
-                <AppBar position="static" color="transparent" elevation={0} m={0} p={0}>
-                  <Tabs
-                    value={phenogenonvalue}
-                    onChange={handleChangePhenogenon}
-                    indicatorColor="primary"
-                    textColor="primary"
-                    variant="fullWidth"
-                    aria-label="full width tabs example"
-                    classes={{ indicator: 'hpo-bigIndicator' }}>
-                    {[t('HPO.RECESSIVE'), t('HPO.DOMINANT')].map((item, index) => {
-                      return <Tab key={index} label={item} {...a11yProps(index)} />;
-                    })}
-                  </Tabs>
-                </AppBar>
-                <SwipeableViews index={phenogenonvalue} onChangeIndex={handleChangePhenogenonIndex}>
-                  <TabPanel className="hpo-tabPannel" value={phenogenonvalue} index={0}>
-                    <VersatileTable
-                      tableData={hpoInfo.phenogenon_recessive}
-                      title={t('Recessive')}
-                      subtitle={[
-                        <Trans i18nKey="HPO.RECESSIVE_subtitle">
-                          <b>Genotype</b> : With at least two variants on a given gene that have
-                          ExAC homozygous count not higher than{' '}
-                          <b style={{ color: '#2E84CF' }}>2</b>, and CADD phred score not lower than{' '}
-                          <b style={{ color: '#2E84CF' }}>15</b>.
-                        </Trans>,
-                      ]}
-                      genomePlot={false}
-                    />
-                  </TabPanel>
-                  <TabPanel className="hpo-tabPannel" value={phenogenonvalue} index={1}>
-                    <VersatileTable
-                      tableData={hpoInfo.phenogenon_dominant}
-                      title={t('Dominant')}
-                      subtitle={[
-                        <Trans i18nKey="HPO.DOMINANT_subtitle">
-                          <b>Genotype</b> : With at least one variant on a given gene that has an
-                          ExAC heterozygous count not higher than ",{' '}
-                          <b style={{ color: '#2E84CF' }}>0.0001</b>, ", and CADD phred score not
-                          lower than ", <b style={{ color: '#2E84CF' }}>15</b>, "."
-                        </Trans>,
-                      ]}
-                      genomePlot={false}
-                    />
-                  </TabPanel>
-                </SwipeableViews>
-              </TabPanel>
-
-              <TabPanel value={value} index={3} className="hpo-tabPannel">
-                <VersatileTable
-                  tableData={hpoInfo.skat}
-                  title={t('HPO.SKAT')}
-                  subtitle={t('HPO.SKAT_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-            </SwipeableViews>
-          </div>
-        </React.Fragment>
-      ) : (
-        <Loading message={t('HPO.message')} />
-      )}
-    </>
+            <TabPanel value={value} index={3} className="hpo-tabPannel">
+              <VersatileTable
+                tableData={hpoInfo.skat}
+                title={t('HPO.SKAT')}
+                subtitle={t('HPO.SKAT_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+          </SwipeableViews>
+        ) : (
+          <>
+            <div className="mt-4 mb-4" />
+            <Skeleton variant="rect" height={150} />
+            <div className="mt-4 mb-4" />
+            <Skeleton variant="rect" height={450} />
+          </>
+        )}
+      </div>
+    </React.Fragment>
   );
 };
 
--- frontend/src/page/Variant.js
+++ frontend/src/page/Variant.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { CssBaseline, AppBar, Tabs, Tab, Container } from '@material-ui/core';
-import Loading from '../components/General/Loading';
+import Skeleton from '@material-ui/lab/Skeleton';
 import { useTranslation } from 'react-i18next';
 import SwipeableViews from 'react-swipeable-views';
 import TabPanel from '../components/Tab/Tabpanel';
@@ -50,92 +50,104 @@ const Variant = (props) => {
   };
 
   return (
-    <>
-      {loaded ? (
-        <React.Fragment>
-          <CssBaseline />
-          <div className="variant-container">
-            <MetaData
-              metadata={variantInfo.metadata}
-              name={variantInfo.metadata.data[0].variant_id[0].display}
-            />
+    <React.Fragment>
+      <CssBaseline />
+      <div className="variant-container">
+        {loaded ? (
+          <MetaData
+            metadata={variantInfo.metadata}
+            name={variantInfo.metadata.data[0].variant_id[0].display}
+          />
+        ) : (
+          <Skeleton height={145} />
+        )}
 
-            <Container maxWidth="xl">
-              <AppBar
-                className="variant-tab_appbar"
-                position="static"
-                color="transparent"
-                elevation={0}
-                m={0}
-                p={0}>
-                <Tabs
-                  value={value}
-                  onChange={handleChange}
-                  indicatorColor="primary"
-                  textColor="primary"
-                  variant="fullWidth"
-                  aria-label="full width tabs example"
-                  classes={{ indicator: 'variant-bigIndicator' }}>
-                  {[
-                    t('Variant.FREQUENCY'),
-                    t('Variant.CONSEQUENCES'),
-                    t('Variant.QUALITY'),
-                    t('Variant.INDIVIDUALS'),
-                    t('Variant.GENOTYPES'),
-                  ].map((item, index) => {
-                    return <Tab key={index} label={item} {...a11yProps(index)} />;
-                  })}
-                </Tabs>
-              </AppBar>
-            </Container>
-            <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
-              <TabPanel value={value} index={0} className="variant-tabPannel">
-                <VersatileTable
-                  tableData={variantInfo.frequency}
-                  title={t('Variant.Frequency')}
-                  subtitle={t('Variant.Frequency_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-              <TabPanel value={value} index={1} className="variant-tabPannel">
-                <VersatileTable
-                  tableData={variantInfo.consequence}
-                  title={t('Variant.Consequences')}
-                  subtitle={t('Variant.Consequences_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-              <TabPanel value={value} index={2} className="variant-tabPannel">
-                <VersatileTable
-                  tableData={variantInfo.quality}
-                  title={t('Variant.Quality')}
-                  subtitle={t('Variant.Quality_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-              <TabPanel value={value} index={3} className="variant-tabPannel">
-                <VersatileTable
-                  tableData={variantInfo.individuals}
-                  title={t('Variant.Individuals')}
-                  subtitle={t('Variant.Individuals_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-              <TabPanel value={value} index={4} className="variant-tabPannel">
-                <VersatileTable
-                  tableData={variantInfo.genotypes}
-                  title={t('Variant.Genotypes')}
-                  subtitle={t('Variant.Genotypes_subtitle')}
-                  genomePlot={false}
-                />
-              </TabPanel>
-            </SwipeableViews>
-          </div>
-        </React.Fragment>
-      ) : (
-        <Loading message={t('Variant.message')} />
-      )}
-    </>
+        {loaded ? (
+          <Container maxWidth="xl">
+            <AppBar
+              className="variant-tab_appbar"
+              position="static"
+              color="transparent"
+              elevation={0}
+              m={0}
+              p={0}>
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="primary"
+                textColor="primary"
+                variant="fullWidth"
+                aria-label="full width tabs example"
+                classes={{ indicator: 'variant-bigIndicator' }}>
+                {[
+                  t('Variant.FREQUENCY'),
+                  t('Variant.CONSEQUENCES'),
+                  t('Variant.QUALITY'),
+                  t('Variant.INDIVIDUALS'),
+                  t('Variant.GENOTYPES'),
+                ].map((item, index) => {
+                  return <Tab key={index} label={item} {...a11yProps(index)} />;
+                })}
+              </Tabs>
+            </AppBar>
+          </Container>
+        ) : (
+          <Skeleton height={200} />
+        )}
+
+        {loaded ? (
+          <SwipeableViews index={value} onChangeIndex={handleChangeIndex}>
+            <TabPanel value={value} index={0} className="variant-tabPannel">
+              <VersatileTable
+                tableData={variantInfo.frequency}
+                title={t('Variant.Frequency')}
+                subtitle={t('Variant.Frequency_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={1} className="variant-tabPannel">
+              <VersatileTable
+                tableData={variantInfo.consequence}
+                title={t('Variant.Consequences')}
+                subtitle={t('Variant.Consequences_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={2} className="variant-tabPannel">
+              <VersatileTable
+                tableData={variantInfo.quality}
+                title={t('Variant.Quality')}
+                subtitle={t('Variant.Quality_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={3} className="variant-tabPannel">
+              <VersatileTable
+                tableData={variantInfo.individuals}
+                title={t('Variant.Individuals')}
+                subtitle={t('Variant.Individuals_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+            <TabPanel value={value} index={4} className="variant-tabPannel">
+              <VersatileTable
+                tableData={variantInfo.genotypes}
+                title={t('Variant.Genotypes')}
+                subtitle={t('Variant.Genotypes_subtitle')}
+                genomePlot={false}
+              />
+            </TabPanel>
+          </SwipeableViews>
+        ) : (
+          <>
+            <div className="mt-4 mb-4" />
+            <Skeleton variant="rect" height={150} />
+            <div className="mt-4 mb-4" />
+            <Skeleton variant="rect" height={450} />
+          </>
+        )}
+      </div>
+    </React.Fragment>
   );
 };
 
