c569403a282ee54b52469cdf0ac9a6c48fae5629
Alan Silva <alanwilter@gmail.com>
2021-06-02 13:47:37 +0200

    Safer variant page for demo (#363)

--- tests/test_variants.py
+++ tests/test_variants.py
@@ -21,7 +21,7 @@ def test_variant(_demo):
     response = variant("14-76156575-A-G")
     assert '"gene_symbol":[{"display":"TTLL5"}],"hgvsc":"ENST00000298832.9:c.412A>G"' in str(response.data), "Critical"
     assert '{"display":"my:PH00008258"' in str(response.data), "Check for 'my:..."
-    assert len(str(response.json)) == 7725
+    assert len(str(response.json)) == 7407
     assert "'cadd_phred': '12.28'," in str(response.json), "Check col frequency data"
     assert "end_href': '14-76156575-A-G'" in str(response.json), "Critical, must be present"
     assert "'gene_id': 'ENSG00000119685'," in str(response.json), "Critical, must be present"
