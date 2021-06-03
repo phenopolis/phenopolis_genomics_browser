a70d6875018ac10acfa608aba992b6fe47068676
IsmailM <ismail.moghul@gmail.com>
2021-06-03 07:00:08 +0100

    update i18next config
    * update depreciated xhr backend to http backend
    * use localstorage for caching
    * turn off dev mode

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,26 +1,32 @@
 import i18n from 'i18next';
-import Backend from 'i18next-xhr-backend';
+import ChainedBackend from "i18next-chained-backend";
+import HttpBackend from "i18next-http-backend";
+import LocalStorageBackend from "i18next-localstorage-backend";
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-  .use(Backend)
-  // detect user language
-  // learn more: https://github.com/i18next/i18next-browser-languageDetector
+  .use(ChainedBackend)
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(initReactI18next)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: 'en',
-    debug: true,
+    // debug: true,
 
+    backend: {
+      backends: [
+        LocalStorageBackend,
+        HttpBackend
+      ],
+      backendOptions: [{
+        expirationTime: 7 * 24 * 60 * 60 * 1000 // 7 days
+      }, {
+        loadPath: '/locales/{{lng}}/{{ns}}.json'
+      }]
+    },
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
-    },
+    }
   });
 
 export default i18n;
