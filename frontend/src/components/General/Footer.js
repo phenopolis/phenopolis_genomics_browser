74f8003b2a3058e2068eef5df0e85dc4a24778fa
IsmailM <ismail.moghul@gmail.com>
2021-06-03 06:38:39 +0100

    fix imports
    both react-router-dom and react-router are sub-packages of the same library

--- frontend/src/components/General/Footer.js
+++ frontend/src/components/General/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Grid, Box, Container, CssBaseline, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
-import { useLocation } from 'react-router';
+import { Link, useLocation } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitterSquare, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
 
